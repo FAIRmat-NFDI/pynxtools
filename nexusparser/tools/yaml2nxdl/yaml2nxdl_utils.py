bdb9651a13e9971b79842792130d4a217deec611
aalbino2 <andrea.albino@physik.hu-berlin.de>
2021-12-06 17:41:55 +0100

    Added click arg parsing and documentation

--- nexusparser/tools/yaml2nxdl/yaml2nxdl_utils.py
+++ nexusparser/tools/yaml2nxdl/yaml2nxdl_utils.py
@@ -8,10 +8,7 @@ Created on Mon Nov 22 17:41:08 2021
 
 import os, sys
 import yaml
-##import punx
-##import json
 from lxml import etree
-#import lxml.etree.ElementTree as ET
 
 
 def nx_name_type_resolving(tmp):
@@ -19,24 +16,20 @@ def nx_name_type_resolving(tmp):
     extracts the eventually custom name {optional_string} an type {nexus_type} from a YML section string.
     YML section string syntax: optional_string(nexus_type)  
     """
-    #tmp = '' #will throw
-    #tmp = '(NXentry)' #will return '', 'NXentry' tuple
-    #tmp = 'condensor(NXem_lens)' #will return 'condensor', 'NXem_lens' tuple
-    #tmp = 'type'
     if tmp.count('(') == 1 and tmp.count(')') == 1: 
         #we can safely assume that every valid YML key resolves
         #either an nx_ (type, base, candidate) class contains only 1 '(' and ')'
         idxStart = tmp.index('(')
         idxEnd = tmp.index(')',idxStart+1)
-        typ = tmp[idxStart+1:idxEnd] #.replace('NX','nx_')
-        nam = tmp.replace('('+typ+')','') #.replace('nx_','NX')+')','')
+        typ = tmp[idxStart+1:idxEnd] 
+        nam = tmp.replace('('+typ+')','')
         return nam, typ
     #or a name for a member
     typ = ''
     nam = tmp
     return nam, typ
 
-
+#https://manual.nexusformat.org/classes/base_classes/index.html#base-class-definitions 
 nx_base_clss = ['NXaperture', 'NXattenuator', 'NXbeam', 'NXbeam_stop', 'NXbending_magnet', 'NXcapillary', 'NXcite', 
     'NXcollection', 'NXcollimator', 'NXcrystal', 'NXcylindrical_geometry', 'NXdata', 'NXdetector', 
     'NXdetector_group', 'NXdetector_module', 'NXdisk_chopper', 'NXentry', 'NXenvironment', 'NXevent_data', 
@@ -47,9 +40,9 @@ nx_base_clss = ['NXaperture', 'NXattenuator', 'NXbeam', 'NXbeam_stop', 'NXbendin
     'NXslit', 'NXsource', 'NXsubentry', 'NXtransformations', 'NXtranslation', 'NXuser', 'NXvelocity_selector',
     'NXxraylens']
 
+#https://manual.nexusformat.org/nxdl-types.html?highlight=nx_number 
 nx_type_keys = ['NX_BINARY', 'NX_BOOLEAN', 'NX_CHAR', 'NX_DATE_TIME', 
                 'NX_FLOAT', 'NX_INT', 'NX_NUMBER', 'NX_POSINT', 'NX_UINT', '']
-#nx_grpnm_tag = '___'
 nx_attr_idnt = '\@'
 nx_unit_idnt = 'unit'
 nx_unit_typs = ['NX_ANGLE',  'NX_ANY', 'NX_AREA', 'NX_CHARGE', 'NX_CROSS_SECTION', 'NX_CURRENT', 'NX_DIMENSIONLESS',
