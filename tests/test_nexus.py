2695194270f99b5ff6111f02c49dbac5608a96e6
sanbrock <sandor.brockhauser@gmail.com>
2021-12-14 01:41:42 +0100

    added Repeats to Groups/Fields/Attributes if uppercase letters are in the name; modified CamelCase to Nexus convention to reflect such Repeats; modified and extended test cases

--- tests/test_nexus.py
+++ tests/test_nexus.py
@@ -28,19 +28,19 @@ from nomad.datamodel import EntryArchive
     pytest.param('base_classes.name', 'nexus_base_classes'),
     pytest.param('base_classes.NXobject.name', 'NXobject'),
     pytest.param('base_classes.NXentry.nx_kind', 'group'),
-    pytest.param('base_classes.NXentry.DefaultAttribute.nx_value.type', str),
+    pytest.param('base_classes.NXentry.defaultAttribute.nx_value.type', str),
     pytest.param('base_classes.NXentry.nx_attribute_default', '*'),
     pytest.param('base_classes.NXentry.NXdataGroup', '*'),
-    pytest.param('base_classes.NXdetector.RealTimeField', '*'),
+    pytest.param('base_classes.NXdetector.real_timeField', '*'),
     pytest.param('base_classes.NXentry.NXdataGroup.nx_optional', True),
-    pytest.param('base_classes.NXentry.nx_group_data.section_def.nx_kind', 'group'),
-    pytest.param('base_classes.NXentry.nx_group_data.section_def.nx_optional', True),
-    pytest.param('base_classes.NXentry.nx_group_data.section_def.nx_name.type', str),
-    pytest.param('base_classes.NXdetector.RealTimeField.name', 'RealTimeField'),
-    pytest.param('base_classes.NXdetector.RealTimeField.nx_type', 'NX_NUMBER'),
-    pytest.param('base_classes.NXdetector.RealTimeField.nx_units', 'NX_TIME'),
-    pytest.param('base_classes.NXdetector.RealTimeField.nx_unit', '*'),
-    pytest.param('base_classes.NXdetector.RealTimeField.nx_value', '*'),
+    pytest.param('base_classes.NXentry.nx_group_DATA.section_def.nx_kind', 'group'),
+    pytest.param('base_classes.NXentry.nx_group_DATA.section_def.nx_optional', True),
+    pytest.param('base_classes.NXentry.nx_group_DATA.section_def.nx_name.type', str),
+    pytest.param('base_classes.NXdetector.real_timeField.name', 'real_timeField'),
+    pytest.param('base_classes.NXdetector.real_timeField.nx_type', 'NX_NUMBER'),
+    pytest.param('base_classes.NXdetector.real_timeField.nx_units', 'NX_TIME'),
+    pytest.param('base_classes.NXdetector.real_timeField.nx_unit', '*'),
+    pytest.param('base_classes.NXdetector.real_timeField.nx_value', '*'),
     pytest.param('applications.NXarpes.NXentryGroup.NXdataGroup.nx_optional', False)
 ])
 def test_assert_nexus_metainfo(path: str, value: Any):
@@ -78,21 +78,21 @@ def test_use_nexus_metainfo():
     archive = EntryArchive()
     archive.nexus = nexus.Nexus()
     archive.nexus.nx_application_arpes = nexus.NXarpes()
-    archive.nexus.nx_application_arpes.nx_group_entry = nexus.NXarpes.NXentryGroup()
-    archive.nexus.nx_application_arpes.nx_group_entry.nx_field_title = nexus.NXarpes.NXentryGroup.TitleField()
-    archive.nexus.nx_application_arpes.nx_group_entry.nx_field_title.nx_value = 'my title'
+    archive.nexus.nx_application_arpes.m_create(nexus.NXarpes.NXentryGroup)
+    archive.nexus.nx_application_arpes.nx_group_ENTRY[0].nx_field_title = nexus.NXarpes.NXentryGroup.titleField()
+    archive.nexus.nx_application_arpes.nx_group_ENTRY[0].nx_field_title.nx_value = 'my title'
 
     # Entry/default is not overwritten in NXarpes. Therefore technically, there is no attribute section
     # nexus.NXarpes.NXentryGroup.DefaultAttribute. We artifically extented inheritence to
     # include inner section/classes. So both options work:
-    # archive.nexus.nx_application_arpes.nx_group_entry.nx_attribute_default = nexus.NXentry.DefaultAttribute()
-    archive.nexus.nx_application_arpes.nx_group_entry.nx_attribute_default = nexus.NXarpes.NXentryGroup.DefaultAttribute()
-    archive.nexus.nx_application_arpes.nx_group_entry.nx_attribute_default.nx_value = 'my default'
+    # archive.nexus.nx_application_arpes.nx_group_ENTRY.nx_attribute_default = nexus.NXentry.DefaultAttribute()
+    archive.nexus.nx_application_arpes.nx_group_ENTRY[0].nx_attribute_default = nexus.NXarpes.NXentryGroup.defaultAttribute()
+    archive.nexus.nx_application_arpes.nx_group_ENTRY[0].nx_attribute_default.nx_value = 'my default'
     # pylint: enable=no-member
 
     archive = EntryArchive.m_from_dict(archive.m_to_dict())
-    assert archive.nexus.nx_application_arpes.nx_group_entry.nx_attribute_default.nx_value == 'my default'
-    assert archive.nexus.nx_application_arpes.nx_group_entry.nx_field_title.nx_value == 'my title'
+    assert archive.nexus.nx_application_arpes.nx_group_ENTRY[0].nx_attribute_default.nx_value == 'my default'
+    assert archive.nexus.nx_application_arpes.nx_group_ENTRY[0].nx_field_title.nx_value == 'my title'
 
     # TODO remove
     # print(json.dumps(archive.m_to_dict(), indent=2))
@@ -118,14 +118,14 @@ def test_use_nexus_metainfo_reflectivly(path):
 
             if kind == 'group':
                 section_definition = parent_definition.all_inner_section_definitions[f'{name_or_value}Group']
-                sub_section_definition = parent_definition.all_sub_sections[f'nx_group_{name_or_value.replace("NX", "")}']
+                sub_section_definition = parent_definition.all_sub_sections[f'nx_group_{name_or_value.replace("NX", "").upper()}']
 
             if kind == 'field':
-                section_definition = parent_definition.all_inner_section_definitions[f'{name_or_value.title()}Field']
+                section_definition = parent_definition.all_inner_section_definitions[f'{name_or_value}Field']
                 sub_section_definition = parent_definition.all_sub_sections[f'nx_field_{name_or_value}']
 
             if kind == 'attribute':
-                section_definition = parent_definition.all_inner_section_definitions[f'{name_or_value.title()}Attribute']
+                section_definition = parent_definition.all_inner_section_definitions[f'{name_or_value}Attribute']
                 sub_section_definition = parent_definition.all_sub_sections[f'nx_attribute_{name_or_value}']
 
             new_object = section_definition.section_cls()
