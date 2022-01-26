634ecde6d0c9d95185c5a31be8588b9fa9861370
Sherjeel Shabih <shabihsherjeel@gmail.com>
2022-01-26 14:45:50 +0100

    Working mpes reader

--- nexusparser/tools/dataconverter/readers/mpes/reader.py
+++ nexusparser/tools/dataconverter/readers/mpes/reader.py
@@ -25,20 +25,21 @@ from nexusparser.tools.dataconverter.readers.base.reader import BaseReader
 import xarray as xr
 
 default_units = {
-    'X': 'step', 
-    'Y': 'step', 
-    't': 'step', 
-    'tofVoltage':'V',
-    'extractorVoltage':'V',
-    'extractorCurrent':'A',
-    'cryoTemperature':'K',
-    'sampleTemperature':'K',
-    'dldTimeBinSize':'ns',
-    'delay':'ps',
-    'timeStamp':'s',
-    'E':'eV',
-    'kx':'1/A',
-    'ky':'1/A'}
+    'X': 'step',
+    'Y': 'step',
+    't': 'step',
+    'tofVoltage': 'V',
+    'extractorVoltage': 'V',
+    'extractorCurrent': 'A',
+    'cryoTemperature': 'K',
+    'sampleTemperature': 'K',
+    'dldTimeBinSize': 'ns',
+    'delay': 'ps',
+    'timeStamp': 's',
+    'E': 'eV',
+    'kx': '1/A',
+    'ky': '1/A'}
+
 
 def res_to_xarray(res, binNames, binAxes, metadata=None):
     """ creates a BinnedArray (xarray subclass) out of the given np.array
@@ -72,7 +73,7 @@ def res_to_xarray(res, binNames, binAxes, metadata=None):
 
     return xres
 
-    
+
 def h5_to_xarray(faddr, mode='r'):
     """ Rear xarray data from formatted hdf5 file
     Args:
@@ -82,58 +83,52 @@ def h5_to_xarray(faddr, mode='r'):
         xarray (xarray.DataArray): output xarra data
     """
     with h5py.File(faddr, mode) as h5File:
-
-        print(f'reading data from {faddr}')
-        
         # Reading data array
         try:
             data = h5File['binned']['BinnedData']
-        except:
+        except KeyError:
             print("Wrong Data Format, data not found")
             raise
-            
+
         # Reading the axes
         axes = []
         binNames = []
-        
+
         try:
             for axis in h5File['axes']:
                 axes.append(h5File['axes'][axis])
                 binNames.append(h5File['axes'][axis].attrs['name'])
-        except:
+        except KeyError:
             print("Wrong Data Format, axes not found")
             raise
 
         # load metadata
         if 'metadata' in h5File:
-            
             def recursive_parse_metadata(node):
                 if isinstance(node, h5py.Group):
                     d = {}
-                    for k,v in node.items():
+                    for k, v in node.items():
                         d[k] = recursive_parse_metadata(v)
-                
-                else: 
+
+                else:
                     d = node[...]
                     try:
                         d = d.item()
                         if isinstance(d, (bytes, bytearray)):
                             d = d.decode()
-                    except:
+                    except ValueError:
                         pass
-                
+
                 return d
-            
+
             metadata = recursive_parse_metadata(h5File['metadata'])
-        
+
         xarray = res_to_xarray(data, binNames, axes, metadata)
         return xarray
 
-        print('Loading complete!')
-
 
 def iterate_dictionary(dic, key_string):
-    keys = key_string.split('/',1)
+    keys = key_string.split('/', 1)
     if keys[0] in dic:
         if len(keys) == 1:
             return dic[keys[0]]
@@ -157,62 +152,48 @@ class MPESReader(BaseReader):
             raise Exception("No input files were given to MPES Reader.")
 
         for file_path in file_paths:
- 
+
             file_extension = file_path[file_path.rindex("."):]
 
             if file_extension == '.h5':
-                x_array_loaded= h5_to_xarray(file_path)
+                x_array_loaded = h5_to_xarray(file_path)
 
             elif file_extension == '.json':
-                with open(file_path,'r') as f:
-                    config_file_dict= json.load(f) 
-                    
-        
-        for k,v in config_file_dict.items():
+                with open(file_path, 'r') as f:
+                    config_file_dict = json.load(f)
+
+        for k, v in config_file_dict.items():
 
             if isinstance(v, str) and ':' in v:
                 precursor = v.split(':')[0]
-                value= v[v.index(':')+1:]
+                value = v[v.index(':') + 1:]
 
-                #Filling in the data and axes along with units from xarray
+                # Filling in the data and axes along with units from xarray
                 if precursor == '@data':
                     try:
                         template[k] = eval("x_array_loaded." + value)
                         if k.split('/')[-1] == '@axes':
                             template[k] = list(template[k])
-                        
-                    except NameError:  
+
+                    except NameError:
                         print(f"Incorrect naming syntax or the xarray doesn't contain entry corresponding to the path {k}")
                     except KeyError:
                         print(f"The xarray doesn't contain entry corresponding to the path {k}")
 
-                #Filling in the metadata from xarray
+                # Filling in the metadata from xarray
                 elif precursor == '@attrs':
 
-                    try: #Tries to fill the metadata 
-                        template[k] = iterate_dictionary(x_array_loaded.attrs, value)  
-                        #print(k)
-                        #print(value)
-                        #print(template[k])
-                        #print(type(template[k]))
+                    try:  # Tries to fill the metadata
+                        template[k] = iterate_dictionary(x_array_loaded.attrs, value)
 
                     except KeyError:
                         print(f"The xarray doesn't contain entry corresponding to the path {k}")
 
-                        
-            else:  
-                #Fills in the fixed metadata
-                
-                template[k] = v 
-                    
-                
-                    
-        print('Template ready!')
-        # print(template)
+            else:
+                # Fills in the fixed metadata
+                template[k] = v
+
         return template
 
 
 READER = MPESReader
-
-
-
