ec2881b22285211be4cd5a9d337c25d99a989a53
Arora0 <abeer.arora.13@gmail.com>
2022-06-03 18:40:53 +0200

    bug fix for python version 3.8

--- nexusparser/tools/dataconverter/readers/mpes/reader.py
+++ nexusparser/tools/dataconverter/readers/mpes/reader.py
@@ -132,10 +132,10 @@ def h5_to_xarray(faddr, mode="r"):
             metadata = recursive_parse_metadata(h5_file["metadata"])
         # Segment to change Vset to V in lens voltages
         if "file" in metadata.keys():
-            for k, v in metadata['file'].items():
+            for k in list(metadata['file']):
                 if "VSet" in k:
                     key = k[:-3]
-                    metadata['file'][key] = v
+                    metadata['file'][key] = metadata['file'][k]
                     del metadata['file'][k]
 
         xarray = res_to_xarray(data, bin_names, axes, metadata)
