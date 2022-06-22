cd5029c6be34b86ee0e4500aeb6f84d36a93e564
Sherjeel Shabih <shabihsherjeel@gmail.com>
2022-06-22 16:31:50 +0200

    Fixed optional parent check

--- nexusparser/tools/dataconverter/helpers.py
+++ nexusparser/tools/dataconverter/helpers.py
@@ -254,8 +254,12 @@ def all_required_children_are_set(optional_parent_path, data, nxdl_root):
 
 def is_nxdl_path_a_child(nxdl_path: str, parent: str):
     """Takes an NXDL path for an element and an NXDL parent and confirms it is a child."""
-    nxdl_stripped_to_parent_length = nxdl_path[0:len(parent)]
-    return nxdl_stripped_to_parent_length == parent
+    while nxdl_path.rfind("/") != -1:
+        nxdl_path = nxdl_path[0:nxdl_path.rfind("/")]
+        if parent == nxdl_path:
+            return True
+    return False
+
 
 def check_optionality_based_on_parent_group(
         path,
@@ -266,7 +270,7 @@ def check_optionality_based_on_parent_group(
     """Checks whether field is part of an optional parent and then confirms its optionality"""
     for optional_parent in template["optional_parents"]:
         optional_parent_nxdl = convert_data_converter_dict_to_nxdl_path(optional_parent)
-        if is_nxdl_path_a_child(nxdl_path, optional_parent) \
+        if is_nxdl_path_a_child(nxdl_path, optional_parent_nxdl) \
            and not all_required_children_are_set(optional_parent, data, nxdl_root):
             raise Exception(f"The data entry, {path}, has an optional parent, "
                             f"{optional_parent}, with required children set. Either"
