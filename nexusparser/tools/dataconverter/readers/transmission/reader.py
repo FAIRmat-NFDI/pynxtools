c2f379fd5aade0198793fb153b58a973b8a85685
domna <florian.dobener@physik.hu-berlin.de>
2022-08-02 23:30:32 +0200

    Adds reading functionality to transmission reader

--- nexusparser/tools/dataconverter/readers/transmission/reader.py
+++ nexusparser/tools/dataconverter/readers/transmission/reader.py
@@ -17,81 +17,103 @@
 #
 """MyDataReader implementation for the DataConverter to convert mydata to Nexus."""
 
-import errno
 import os
+import collections
 from typing import Tuple, Any, Dict
 import json
 import yaml
+import pandas as pd
 
 from nexusparser.tools.dataconverter.readers.base.reader import BaseReader
 
 
-def parse_asc(file_path: str) -> Dict[str, Any]:
-    pass
+#: Dictionary mapping the line numbers in the asc file to the paths in the NeXus file.
+METADATA_MAP: Dict[str, Any] = {}
+
+
+def parse_asc(file_path: str, encoding: str = "utf-8") -> Dict[str, Any]:
+    """Parses a Perkin Ellmer asc file into metadata and data dictionary.
+
+    Args:
+        file_path (str): File path to the asc file.
+
+    Returns:
+        Dict[str, Any]: Dictionary containing the metadata and data from the asc file.
+    """
+    template: Dict[str, Any] = {}
+    data_start_ind = "#DATA"
+
+    with open(file_path, encoding=encoding) as fobj:
+        keys = []
+        for line in fobj:
+            if line.strip() == data_start_ind:
+                break
+            keys.append(line.strip())
+
+        for key, val in METADATA_MAP.items():
+            template[key] = keys[val]
+
+        template["/ENTRY[entry]/data"] = pd.read_csv(
+            fobj, delim_whitespace=True, header=None, index_col=0
+        )
+
+    return template
 
 
 def parse_json(file_path: str) -> Dict[str, Any]:
+    """Parses a metadata json file into a dictionary.
+
+    Args:
+        file_path (str): The file path of the json file.
+
+    Returns:
+        Dict[str, Any]: The dictionary containing the data readout from the json.
+    """
     with open(file_path, "r") as file:
         return json.load(file)
 
 
+CONVERT_DICT: Dict[str, str] = {}
+REPLACE_NESTED: Dict[str, str] = {}
+
+
+def flatten_and_replace(dic, parent_key="/ENTRY[entry]", sep="/"):
+    """Flatten a nested dictionary, and replace the keys with the appropriate
+    paths in the nxs file.
+    Args:
+        d (dict): dictionary to flatten
+        parent_key (str): parent key of the dictionary
+        sep (str): separator for the keys
+    Returns:
+        dict: flattened dictionary
+    """
+    items = []
+    for key, val in dic.items():
+        new_key = parent_key + sep + CONVERT_DICT.get(key, key)
+        if isinstance(val, collections.Mapping):
+            items.extend(flatten_and_replace(val, new_key, sep=sep).items())
+        else:
+            for old, new in REPLACE_NESTED.items():
+                new_key = new_key.replace(old, new)
+
+            if new_key.endswith("/value"):
+                items.append((new_key[:-6], val))
+            else:
+                items.append((new_key, val))
+    return dict(items)
+
+
 def parse_yml(file_path: str) -> Dict[str, Any]:
-    with open(file_path) as file:
-        return yaml.safe_load(file)
-
-
-def load_files_into_template(file_paths: Tuple[str]) -> Dict[str, Any]:
-    """_summary_
+    """Parses a metadata yaml file into a dictionary.
 
     Args:
-        file_paths (Tuple[str]): _description_
-
-    Raises:
-        ValueError: _description_
-        FileNotFoundError: _description_
+        file_path (str): The file path of the yml file.
 
     Returns:
-        list: _description_
+        Dict[str, Any]: The dictionary containing the data readout from the yml.
     """
-    extensions = {
-        ".asc": parse_asc,
-        ".json": parse_json,
-        ".yml": parse_yml,
-        ".yaml": parse_yml,
-    }
-
-    # Approach:
-    # 1. Split files by extensions, if not splitable throw a warning and ignore file
-    # 2. Check if file exists, if not throw a warning and ignore file
-    # 3. Sort files by extension (to account for reproducible updating of reduntant values)
-    # 4. Loop through files list
-
-    data: Dict[str, Any] = {}
-    for file_path in file_paths:
-        try:
-            file_extension = file_path[file_path.rindex(".") :]
-        except ValueError as err:
-            raise ValueError(
-                f"The file path {file_path} muste have an extension."
-            ) from err
-
-        if file_extension not in extensions.keys():
-            print(
-                f"WARNING\n"
-                f"The reader only supports files of type {extensions.keys()},"
-                f"but {file_path} does not match."
-            )
-
-        if not os.path.exists(file_path):
-            raise FileNotFoundError(
-                errno.ENOENT,
-                os.strerror(errno.ENOENT),
-                file_path,
-            )
-
-        data.update(extensions.get(file_extension, lambda _: {})(file_path))
-
-    return data
+    with open(file_path) as file:
+        return flatten_and_replace(yaml.safe_load(file))
 
 
 class TransmissionReader(BaseReader):
@@ -103,7 +125,7 @@ class TransmissionReader(BaseReader):
         self,
         template: dict = None,
         file_paths: Tuple[str] = None,
-        objects: Tuple[Any] = None,
+        _: Tuple[Any] = None,
     ) -> dict:
         """_summary_
 
@@ -116,8 +138,30 @@ class TransmissionReader(BaseReader):
             dict: _description_
         """
 
-        if not file_paths:
-            raise Exception("No input files were given to the transmission reader.")
+        extensions = {
+            ".asc": lambda fpath: parse_asc(fpath),
+            ".json": parse_json,
+            ".yml": parse_yml,
+            ".yaml": parse_yml,
+        }
+
+        if template is None:
+            template = {}
+
+        sorted_paths = sorted(file_paths, key=lambda f: os.path.splitext(f)[1])
+        for file_path in sorted_paths:
+            extension = os.path.splitext(file_path)[1]
+            if extension not in extensions.keys():
+                print(
+                    f"WARNING: "
+                    f"File {file_path} has an unsupported extension, ignoring file."
+                )
+                continue
+            if not os.path.exists(file_path):
+                print(f"WARNING: File {file_path} does not exist, ignoring entry.")
+                continue
+
+            template.update(extensions.get(extension, lambda _: {})(file_path))
 
         return template
 
