b4d6d7ecbecbb9908f57eab6bbaa22f1d612b49d
RubelMozumder <rubelmozumder@outlook.com>
2022-11-18 18:16:51 +0100

    Ready to try config_file.json for data from xml data file.

--- nexusutils/dataconverter/readers/xps_specs/reader.py
+++ nexusutils/dataconverter/readers/xps_specs/reader.py
@@ -18,11 +18,13 @@
 """A generic reader for loading XPS (X-ray Photoelectron Spectroscopy) data
  file into nxdl (NeXus Definition Language) template.
 """
+import os.path
 
 from nexusutils.dataconverter.readers.base.reader import BaseReader
 from typing import Tuple
 
 from typing import Any, List
+import xarray as xr
 import numpy as np
 import sys
 from nexusutils.dataconverter.readers.xps_specs import XpsDataFileParser
@@ -35,6 +37,8 @@ def find_entry_and_value(xps_data_dict,
                          data_dest, #TODO rewrite the data dest as data_loc
                          dest_type):
 
+    entries_values = dict()
+
     if dest_type in ["@region_data", "@parameters_data"]:
         for key, val in xps_data_dict.items():
             if data_dest in key:
@@ -43,8 +47,7 @@ def find_entry_and_value(xps_data_dict,
                          components[2].split("_", 1)[1] + ":" +
                          components[4].split("_", 1)[1]
                          )
-            del data_dest[key]
-            entries_values = {entry: val}
+                entries_values = {entry: val}
 
     if dest_type=="@data":
         for key, val in xps_data_dict.items()():
@@ -54,19 +57,27 @@ def find_entry_and_value(xps_data_dict,
                          components[2].split("_", 1)[1] + ":" +
                          components[4].split("_", 1)[1]
                          )
+
+                if entry not in entries_values.keys():
+                    entries_values[entry] = xr.Dataset()
+
+                # data_dest -> cycles/Cycle_
                 _, last_part = key.split(data_dest)
-                scan_num, counts = last_part.split("/")[-2:]
-                # TODO use Xarray data type here
-            entries_values = {entry: {}}
+                parts = last_part.split("/")
+                cycle_num, scan_num = parts[0], parts[-2].split("_")[1]
+                da_name = f"cycle{cycle_num}_scan{scan_num}"
 
+                entries_values[entry][da_name] = val
 
+                # TODO add x-coordinate if the the BE can be calculated later
 
     return entries_values
 
 
 class XPS_Reader(BaseReader):
 
-    supported_nxdls = ["NXroot"]
+    supported_nxdls = ["NXmpes"]
+
 
     def read(self,
              template: dict = None,
@@ -74,6 +85,10 @@ class XPS_Reader(BaseReader):
              objects: Tuple[Any] = None) -> dict:
         """Reads data from given file and returns a filled template dictionary"""
 
+        # TODO: add the config file in example folder or intended folder
+        # (if the is not eample folder) and add the path in con_file_path
+        con_file_path = f'{os.path.dirname("__file__")}{os.path.sep}cofig_file.json'
+        file_paths.append(con_file_path)
         config_dict = {}
         xps_data_dict = {}
 
@@ -107,6 +122,8 @@ class XPS_Reader(BaseReader):
                     try:
                         template[f"{modified_key}/@units"] = \
                             config_dict[f"{key}/@units"]
+
+                        del template[f"{key}/@units"]
                     except KeyError:
                         pass
 
@@ -128,6 +145,8 @@ class XPS_Reader(BaseReader):
                     try:
                         template[f"{modified_key}/@units"] = \
                             config_dict[f"{key}/@units"]
+
+                        del template[f"{key}/@units"]
                     except KeyError:
                         pass
 
@@ -149,31 +168,35 @@ class XPS_Reader(BaseReader):
                 for entry, value in entries_values.items():
                     modified_key = key.replace("entry", entry)
                     template[modified_key] = value
-                    # template[f"{modified_key}\@signal"] = TODO: Finishe it
-                    template[f"{modified_key}/@units"] = config_dict[f"{key}/@units"]
+                    # signal takes the sum of the counts along the scan axis
+                    template[f"{modified_key}/@signal"] = \
+                        np.sum([value[x_arr] for x_arr in value.data_vars],
+                               axis=0)
+                    template[f"{modified_key}/@units"] = \
+                        config_dict[f"{key}/@units"]
                 try:
                     del template[f"{key}"]
-                    del template[f"{key}/signal"]
-                    del template[f"{key}/units"]
+                    del template[f"{key}/@signal"]
+                    del template[f"{key}/@units"]
                 except KeyError:
                     pass
 
-        if not template.items():
-            # intended for NXroot
-            for key, val in data_dict.items():
-                if key[-1] == "/":
-                    key = key[:-1]
-                if val not in ["None"]:
-                    template[key] = val
-
-        elif template.items():
-            # intended for NXtest
-            for key, val in data_dict.items():
-                if key[-1] == "/":
-                    key = key[:-1]
-
-                if val not in ["None"]:
-                    template[key] = val
+        # if not template.items():
+        #     # intended for NXroot
+        #     for key, val in data_dict.items():
+        #         if key[-1] == "/":
+        #             key = key[:-1]
+        #         if val not in ["None"]:
+        #             template[key] = val
+        #
+        # elif template.items():
+        #     # intended for NXtest
+        #     for key, val in data_dict.items():
+        #         if key[-1] == "/":
+        #             key = key[:-1]
+        #
+        #         if val not in ["None"]:
+        #             template[key] = val
 
         return template
 
