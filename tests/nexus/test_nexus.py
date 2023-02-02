d4a29d4535824d94df7ef41f8d27560ebbbf7740
markus.kuehbach <markus.kuehbach@hu-berlin.de>
2023-02-02 12:42:23 +0100

    fixing assert

--- tests/nexus/test_nexus.py
+++ tests/nexus/test_nexus.py
@@ -119,7 +119,7 @@ def test_get_node_at_nxdl_path():
 
     node = nexus.get_node_at_nxdl_path(
         "/ENTRY/measurement/EVENT_DATA_EM/SPECTRUM_SET_EM_XRAY/summary/DATA", elem=elem)
-    assert node.attrib["type"] == "NXdata"
+    assert node.attrib["type"] == "NX_NUMBER"
 
     node = nexus.get_node_at_nxdl_path(
         "/ENTRY/measurement/EVENT_DATA_EM/SPECTRUM_SET_EM_XRAY/summary/AXISNAME_indices",
