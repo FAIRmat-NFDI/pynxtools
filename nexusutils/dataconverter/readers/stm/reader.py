e98642efdcb690da918b6d0a2ba02a7b761d6ead
RubelMozumder <rubelmozumder@outlook.com>
2023-04-21 12:32:47 +0200

    Remoiving changes from commit 'Including nanonispy in pyproject.toml.'.

--- nexusutils/dataconverter/readers/stm/reader.py
+++ nexusutils/dataconverter/readers/stm/reader.py
@@ -20,26 +20,19 @@
 
 
 import os
-from pathlib import Path
-from typing import Any, Dict, Set, List,Tuple
+from typing import Any, Dict, List, Tuple
 from typing import Tuple
 import numpy as np
 import nanonispy as nap
 import re
 
-import sys
-import json
-
-from nexusutils.dataconverter.readers.utils import flatten_and_replace, FlattenSettings
 from nexusutils.dataconverter.readers.base.reader import BaseReader
 from nexusutils.dataconverter.template import Template
 
 
-
-
 def is_separator_char_exist(key, sep_char_li):
     """
-    Check string or key whether the separator char provided in 
+    Check string or key whether the separator char provided in
     'Separator Char List' exist or not.
     """
     bool_k = [x in sep_char_li for x in key]
@@ -48,17 +41,16 @@ def is_separator_char_exist(key, sep_char_li):
 
 def get_nested_dict_from_nested_key_dict(data_dict, dict_to_map_path=None):
     """
-    Create nested dict. If key are concateneted with '_', '>' split the key and 
-    construct nested dict. For example, {'x1': {'x2': {'x3': {'x4': {'x5': 3}}}} 
+    Create nested dict. If key are concateneted with '_', '>' split the key and
+    construct nested dict. For example, {'x1': {'x2': {'x3': {'x4': {'x5': 3}}}}
     from 'x1_x2_x3_x4>x5:3'
     """
     if dict_to_map_path is not None:
         spreaded_dict = dict_to_map_path
     else:
-        spreaded_dict: Dict[str, Any] = {} 
+        spreaded_dict: Dict[str, Any] = {}
     sep_chars = ['_', '>']
     for d_key, d_val in data_dict.items():
-        #any_sep_char = [x in d_key for x in separared_char]
         if is_separator_char_exist(d_key, sep_chars):
             # Find out which separator char exist there
             for k_c in d_key:
@@ -68,16 +60,16 @@ def get_nested_dict_from_nested_key_dict(data_dict, dict_to_map_path=None):
             l_key, r_key = d_key.split(sep_char, 1)
             if not is_separator_char_exist(r_key, sep_chars):
                 if l_key not in spreaded_dict:
-                    spreaded_dict[l_key]: Dict[str, Any] = {}    
+                    spreaded_dict[l_key]: Dict[str, Any] = {}
                 spreaded_dict[l_key][r_key] = d_val
             else:
                 if l_key in spreaded_dict:
                     spreaded_dict[l_key] = get_nested_dict_from_nested_key_dict(
-                        {r_key:d_val}, dict_to_map_path=spreaded_dict[l_key])
+                        {r_key: d_val}, dict_to_map_path=spreaded_dict[l_key])
                 else:
                     spreaded_dict[l_key]: Dict[str, Any] = {}
                     spreaded_dict[l_key] = get_nested_dict_from_nested_key_dict(
-                        {r_key:d_val}, dict_to_map_path=spreaded_dict[l_key])
+                        {r_key: d_val}, dict_to_map_path=spreaded_dict[l_key])
         else:
             spreaded_dict[d_key] = d_val
 
@@ -93,8 +85,8 @@ def convert_key_to_unit_and_entity(key, val, start_bracket='', end_bracket=''):
             tmp_l_part, tmp_r_part = key.rsplit(start_bracket)
             unit = tmp_r_part.rsplit(end_bracket)[0]
             raw_key = tmp_l_part.strip()
-            
-            return [(raw_key,val), (f"{raw_key}/@units", unit)]
+
+            return [(raw_key, val), (f"{raw_key}/@units", unit)]
     return
 
 
@@ -116,14 +108,14 @@ def dict_to_path(nested_dict, full_path='', sep='/'):
 def sxm_raw_metadata_and_signal(file_name):
     """
     Retun metadata plain dict and signal
-    Convert header part (that contains metadata) of a file with 'sxm' extension into 
-    plain dict. 
+    Convert header part (that contains metadata) of a file with 'sxm' extension into
+    plain dict.
     """
     print(file_name)
-    
+
     scan_file = nap.read.Scan(file_name)
     header_end_byte = scan_file.start_byte()
-    
+
     h_part = scan_file.read_raw_header(header_end_byte)
     while True:
         if not re.match("[a-zA-Z]", h_part):
@@ -131,7 +123,7 @@ def sxm_raw_metadata_and_signal(file_name):
         else:
             break
     h_comp_iter = iter(re.split('\n:|:\n', h_part))
-    
+
     return dict(zip(h_comp_iter, h_comp_iter)), scan_file.signals
 
 
@@ -145,7 +137,7 @@ def get_SPM_metadata_dict_and_signal(file_name):
     entity_unit_items: List[Tuple[Any, Any]] = []
     for path, val in path_items:
         phy_qunt_unit_list = convert_key_to_unit_and_entity(path, val,
-                                                            start_bracket='(', 
+                                                            start_bracket='(',
                                                             end_bracket=')')
         if phy_qunt_unit_list is not None:
             entity_unit_items.extend(phy_qunt_unit_list)
@@ -162,13 +154,13 @@ def pass_metadata_and_signal_into_template(template, file_name):
     """
 
     dict_path, signal = get_SPM_metadata_dict_and_signal(file_name)
-    
+
     for key, val in dict_path:
         key = '/ENTRY[Entry]' + key
         template[key] = val
     template.update(signal_obj_to_template_dict(signal))
     return template
-    
+
 
 def signal_obj_to_template_dict(signal):
     """
@@ -176,17 +168,16 @@ def signal_obj_to_template_dict(signal):
     """
     template_dict = {}
 
-    
     def construct_nexus_data_path(nx_data_field, np_data, prefix,
-                                axes=None, signal=False):
-    
+                                  axes=None, signal=False):
+
         key = prefix + '/' + nx_data_field
-        tmp_dict = {key:np_data}
+        tmp_dict = {key: np_data}
         if signal:
             key = prefix + '/@signal'
             tmp_dict[key] = nx_data_field[1:]
         if axes:
-            key = prefix + '/'+ nx_data_field + '/@axes'
+            key = prefix + '/' + nx_data_field + '/@axes'
             tmp_dict[key] = axes
             for i, axis in enumerate(axes):
                 key = prefix + '/' + nx_data_field + f'/@{axis}_indices'
@@ -201,11 +192,12 @@ def signal_obj_to_template_dict(signal):
         else:
             nxdata_signal = False
 
-        template_dict.update(construct_nexus_data_path(nx_full_path,
-                                            val, '/ENTRY[Entry]/DATA[DATA]',
-                                            ['slow\u00A0scan\u00A0direction\u00A0in\u00A0pixel', 
-                                            'fast\u00A0scan\u00A0direction\u00A0in\u00A0pixel'],
-                                            signal=nxdata_signal))
+        template_dict.update(construct_nexus_data_path(
+            nx_full_path,
+            val, '/ENTRY[Entry]/DATA[DATA]',
+            ['slow\u00A0scan\u00A0direction\u00A0in\u00A0pixel',
+             'fast\u00A0scan\u00A0direction\u00A0in\u00A0pixel'],
+            signal=nxdata_signal))
 
     return template_dict
 
@@ -213,21 +205,24 @@ def signal_obj_to_template_dict(signal):
 class STMReader(BaseReader):
     """ Reader for XPS.
     """
-   # NXroot is a general purpose definition one can review data with this definition
+    # NXroot is a general purpose definition one can review data with this definition
     supported_nxdls = ["NXroot"]
 
     def read(self,
              template: dict = None,
              file_paths: Tuple[str] = None,
-             objects: Tuple[Any] = None) -> dict:
+             objects: Tuple[Any] = None):
         """
+            General read menthod to prepare the template.
         """
-        file_name = ('/home/rubel/Nomad-FAIRmat/NomadGH/GH_Clone/nomad-parser-nexus/nexusutils/'
-                     'dataconverter/readers/stm/SPM_data_folder/'
-                     'TiSe2_2303a_annealing_300C_5min_evaporate_Pyrene_1_0070.sxm')
+
+        file_dir = os.path.abspath(os.path.dirname(__file__))
+        SPM_file = os.path.join(
+            file_dir,
+            'SPM_data_folder/TiSe2_2303a_annealing_300C_5min_evaporate_Pyrene_1_0070.sxm')
 
         clean_template = Template()
-        filled_template = pass_metadata_and_signal_into_template(clean_template, file_name)
+        filled_template = pass_metadata_and_signal_into_template(clean_template, SPM_file)
         return filled_template
 
 
