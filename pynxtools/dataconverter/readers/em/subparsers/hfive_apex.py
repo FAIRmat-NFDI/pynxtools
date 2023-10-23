Bug fixing, implementation for normalization old edax, community hebsd, and tested

pynxtools/dataconverter/readers/em/subparsers/hfive_apex.py
@@ -34,11 +34,8 @@ from orix.vector import Vector3d
 import matplotlib.pyplot as plt
 
 from pynxtools.dataconverter.readers.em.subparsers.hfive_base import HdfFiveBaseParser
-from pynxtools.dataconverter.readers.em.utils.hfive_utils import read_strings_from_dataset
-
-
-def om_eu(inp):
-    return inp[0:2]
+from pynxtools.dataconverter.readers.em.utils.hfive_utils import \
+    read_strings_from_dataset, format_euler_parameterization
 
 
 class HdfFiveEdaxApexReader(HdfFiveBaseParser):
@@ -63,21 +60,22 @@ class HdfFiveEdaxApexReader(HdfFiveBaseParser):
 
     def check_if_supported(self):
         """Check if instance matches all constraints to qualify as supported H5OINA"""
-        self.supported = True  # try to falsify
+        self.supported = 0  # voting-based
         with h5py.File(self.file_path, "r") as h5r:
             # parse Company and PRODUCT_VERSION attribute values from the first group below / but these are not scalar but single value lists
             # so much about interoperability
             # but hehe for the APEX example from Sebastian and Sabine there is again no Company but PRODUCT_VERSION, 2 files, 2 "formats"
             grp_names = list(h5r["/"])
             if len(grp_names) == 1:
-                if read_strings_from_dataset(h5r[grp_names[0]].attrs["Company"][0]) \
-                    not in self.supported_version["tech_partner"]:
-                    self.supported = False
-                if read_strings_from_dataset(h5r[grp_names[0]].attrs["PRODUCT_VERSION"][0]) \
-                    not in self.supported_version["schema_version"]:
-                    self.supported = False
-            if self.supported is True:
+                if read_strings_from_dataset(h5r[grp_names[0]].attrs["Company"][0]) in self.supported_version["tech_partner"]:
+                    self.supported += 1
+                if read_strings_from_dataset(h5r[grp_names[0]].attrs["PRODUCT_VERSION"][0]) in self.supported_version["schema_version"]:
+                    self.supported += 1
+            if self.supported == 2:
                 self.version = self.supported_version.copy()
+                self.supported = True
+            else:
+                self.supported = False
 
     def parse_and_normalize(self):
         """Read and normalize away EDAX/APEX-specific formatting with an equivalent in NXem."""
@@ -119,13 +117,15 @@ class HdfFiveEdaxApexReader(HdfFiveBaseParser):
                 raise ValueError(f"Unable to parse {self.prfx}/Sample/{req_field} !")
 
         grid_type = read_strings_from_dataset(fp[f"{self.prfx}/Sample/Grid Type"][()])
-        if grid_type != "HexGrid":
+        if grid_type not in ["HexGrid", "SqrGrid"]:
             raise ValueError(f"Grid Type {grid_type} is currently not supported !")
+        self.tmp[ckey]["grid_type"] = grid_type
         self.tmp[ckey]["s_x"] = fp[f"{self.prfx}/Sample/Step X"][0]
         self.tmp[ckey]["s_unit"] = "µm"  # TODO::always micron?
         self.tmp[ckey]["n_x"] = fp[f"{self.prfx}/Sample/Number Of Columns"][0]
         self.tmp[ckey]["s_y"] = fp[f"{self.prfx}/Sample/Step Y"][0]
         self.tmp[ckey]["n_y"] = fp[f"{self.prfx}/Sample/Number Of Rows"][0]
+        # TODO::check that all data are consistent
 
     def parse_and_normalize_group_ebsd_phases(self, fp, ckey: str):
         grp_name = f"{self.prfx}/EBSD/ANG/HEADER/Phase"
@@ -192,44 +192,46 @@ class HdfFiveEdaxApexReader(HdfFiveBaseParser):
 
     def parse_and_normalize_group_ebsd_data(self, fp, ckey: str):
         grp_name = f"{self.prfx}/EBSD/ANG/DATA/DATA"
-        n_pts = self.tmp[ckey]["n_x"] * self.tmp[ckey]["n_y"]
-        if f"{grp_name}" in fp:
-            if np.shape(fp[f"{grp_name}"]) != (n_pts,) and n_pts > 0:
-                raise ValueError(f"Unexpected shape of {grp_name} !")
-
-            dat = fp[f"{grp_name}"]
-            self.tmp[ckey]["euler"] = np.zeros((n_pts, 3), np.float32)
-            # index of phase, 0 if not indexed
-            # # no normalization needed, also in NXem_ebsd the null model notIndexed is phase_identifier 0
-            self.tmp[ckey]["phase_id"] = np.zeros((n_pts,), np.int32)
-            self.tmp[ckey]["ci"] = np.zeros((n_pts,), np.float32)
-
-            for i in np.arange(0, n_pts):
-                # check shape of internal virtual chunked number array
-                r = Rotation.from_matrix([np.reshape(dat[i][0], (3, 3))])
-                self.tmp[ckey]["euler"][i, :] = r.to_euler(degrees=False)
-                self.tmp[ckey]["phase_id"][i] = dat[i][2]
-                self.tmp[ckey]["ci"][i] = dat[i][3]
-
-            # TODO::convert orientation matrix to Euler angles via om_eu but what are conventions !
-            # orix based transformation ends up in positive half space and with degrees=False
-            # as radiants but the from_matrix command above might miss one rotation
-
-            # inconsistency f32 in file although specification states float
-            # Rotation.from_euler(euler=fp[f"{grp_name}/Euler"],
-            #                                 direction='lab2crystal',
-            #                                degrees=is_degrees)
-
-            # compute explicit hexagon grid cells center of mass pixel positions
-            # TODO::currently assuming HexGrid
-            self.tmp[ckey]["scan_point_x"] = np.asarray(
-                np.linspace(0, self.tmp[ckey]["n_x"] - 1,
-                            num=self.tmp[ckey]["n_x"],
-                            endpoint=True) * self.tmp[ckey]["s_x"] + 0., np.float32)
-
-            self.tmp[ckey]["scan_point_y"] = np.asarray(
-                np.linspace(0, self.tmp[ckey]["n_y"] - 1,
-                            num=self.tmp[ckey]["n_y"],
-                            endpoint=True) * self.tmp[ckey]["s_y"] + 0., np.float32)
-        else:
+        if f"{grp_name}" not in fp:
             raise ValueError(f"Unable to parse {grp_name} !")
+
+        n_pts = self.tmp[ckey]["n_x"] * self.tmp[ckey]["n_y"]
+        if np.shape(fp[f"{grp_name}"]) != (n_pts,) and n_pts > 0:
+            raise ValueError(f"Unexpected shape of {grp_name} !")
+
+        dat = fp[f"{grp_name}"]
+        self.tmp[ckey]["euler"] = np.zeros((n_pts, 3), np.float32)
+        # index of phase, 0 if not indexed
+        # # no normalization needed, also in NXem_ebsd the null model notIndexed is phase_identifier 0
+        self.tmp[ckey]["phase_id"] = np.zeros((n_pts,), np.int32)
+        self.tmp[ckey]["ci"] = np.zeros((n_pts,), np.float32)
+
+        for i in np.arange(0, n_pts):
+            # check shape of internal virtual chunked number array
+            r = Rotation.from_matrix([np.reshape(dat[i][0], (3, 3))])
+            self.tmp[ckey]["euler"][i, :] = r.to_euler(degrees=False)
+            self.tmp[ckey]["phase_id"][i] = dat[i][2]
+            self.tmp[ckey]["ci"][i] = dat[i][3]
+
+        # TODO::convert orientation matrix to Euler angles via om_eu but what are conventions !
+        # orix based transformation ends up in positive half space and with degrees=False
+        # as radiants but the from_matrix command above might miss one rotation
+
+        # inconsistency f32 in file although specification states float
+        # Rotation.from_euler(euler=fp[f"{grp_name}/Euler"],
+        #                                 direction='lab2crystal',
+        #                                degrees=is_degrees)
+
+        # compute explicit hexagon grid cells center of mass pixel positions
+        # TODO::currently assuming s_x and s_y are already the correct center of mass
+        # distances for hexagonal or square tiling of R^2
+        # self.tmp[ckey]["grid_type"] in ["HexGrid", "SqrGrid"]:
+        self.tmp[ckey]["scan_point_x"] = np.asarray(
+            np.linspace(0, self.tmp[ckey]["n_x"] - 1,
+                        num=self.tmp[ckey]["n_x"],
+                        endpoint=True) * self.tmp[ckey]["s_x"] + 0., np.float32)
+
+        self.tmp[ckey]["scan_point_y"] = np.asarray(
+            np.linspace(0, self.tmp[ckey]["n_y"] - 1,
+                        num=self.tmp[ckey]["n_y"],
+                        endpoint=True) * self.tmp[ckey]["s_y"] + 0., np.float32)

pynxtools/dataconverter/readers/em/subparsers/hfive_bruker.py
@@ -36,14 +36,8 @@ from orix.vector import Vector3d
 import matplotlib.pyplot as plt
 
 from pynxtools.dataconverter.readers.em.subparsers.hfive_base import HdfFiveBaseParser
-from pynxtools.dataconverter.readers.em.utils.hfive_utils import read_strings_from_dataset
-
-
-def all_equal(iterable):
-    g = groupby(iterable)
-    return next(g, True) and not next(g, False)
-
-BRUKER_MAP_SPACEGROUP = {"F m#ovl3m": 225}
+from pynxtools.dataconverter.readers.em.utils.hfive_utils import EBSD_MAP_SPACEGROUP, \
+    read_strings_from_dataset, all_equal, format_euler_parameterization
 
 
 class HdfFiveBrukerEspritReader(HdfFiveBaseParser):
@@ -68,27 +62,28 @@ class HdfFiveBrukerEspritReader(HdfFiveBaseParser):
 
     def check_if_supported(self):
         """Check if instance matches all constraints to qualify as supported Bruker H5"""
-        self.supported = True  # try to falsify
+        self.supported = 0  # voting-based
         with h5py.File(self.file_path, "r") as h5r:
-            if "/Manufacturer" in h5r:
-                self.version["tech_partner"] \
-                    = read_strings_from_dataset(h5r["/Manufacturer"][()])
-                if self.version["tech_partner"] not in self.supported_version["tech_partner"]:
+            req_fields = ["Manufacturer", "Version"]
+            for req_field in req_fields:
+                if f"/{req_field}" not in h5r:
                     self.supported = False
-            else:
-                self.supported = False
-            if "/Version" in h5r:
-                self.version["schema_version"] \
-                    = read_strings_from_dataset(h5r["/Version"][()])
-                if self.version["schema_version"] not in self.supported_version["schema_version"]:
-                    self.supported = False
-            else:
-                self.supported = False
+                    return
 
-        if self.supported is True:
-            self.version["schema_name"] = self.supported_version["schema_name"]
-            self.version["writer_name"] = self.supported_version["writer_name"]
-            self.version["writer_version"] = self.supported_version["writer_version"]
+            self.version["tech_partner"] = read_strings_from_dataset(h5r["/Manufacturer"][()])
+            if self.version["tech_partner"] in self.supported_version["tech_partner"]:
+                self.supported += 1
+            self.version["schema_version"] = read_strings_from_dataset(h5r["/Version"][()])
+            if self.version["schema_version"] in self.supported_version["schema_version"]:
+                self.supported += 1
+
+            if self.supported == 2:
+                self.version["schema_name"] = self.supported_version["schema_name"]
+                self.version["writer_name"] = self.supported_version["writer_name"]
+                self.version["writer_version"] = self.supported_version["writer_version"]
+                self.supported = True
+            else:
+                self.supported = False
 
     def parse_and_normalize(self):
         """Read and normalize away Bruker-specific formatting with an equivalent in NXem."""
@@ -107,171 +102,135 @@ class HdfFiveBrukerEspritReader(HdfFiveBaseParser):
 
     def parse_and_normalize_group_ebsd_header(self, fp, ckey: str):
         grp_name = f"{self.prfx}/EBSD/Header"
-        if f"{grp_name}/NCOLS" in fp:  # TODO::what is y and x depends on coordinate system
-            self.tmp[ckey]["n_x"] = fp[f"{grp_name}/NCOLS"][()]
-        else:
-            raise ValueError(f"Unable to parse {grp_name}/NCOLS !")
+        if f"{grp_name}" not in fp:
+            raise ValueError(f"Unable to parse {grp_name} !")
 
-        if f"{grp_name}/NROWS" in fp:
-            self.tmp[ckey]["n_y"] = fp[f"{grp_name}/NROWS"][()]
-        else:
-            raise ValueError(f"Unable to parse {grp_name}/NROWS !")
+        req_fields = ["NCOLS", "NROWS", "SEPixelSizeX", "SEPixelSizeY"]
+        for req_field in req_fields:
+            if f"{grp_name}/{req_field}" not in fp:
+                raise ValueError(f"Unable to parse {grp_name}/{req_field} !")
 
-        if f"{grp_name}/SEPixelSizeX" in fp:
-            self.tmp[ckey]["s_x"] = fp[f"{grp_name}/SEPixelSizeX"][()]
-            self.tmp[ckey]["s_unit"] = "µm"  # TODO::always micron?
-        else:
-            raise ValueError(f"Unable to parse {grp_name}/SEPixelSizeX !")
-
-        if f"{grp_name}/SEPixelSizeY" in fp:
-            self.tmp[ckey]["s_y"] = fp[f"{grp_name}/SEPixelSizeY"][()]
-        else:
-            raise ValueError(f"Unable to parse {grp_name}/SEPixelSizeY !")
-        # TODO::check that all data in the self.oina are consistent
+        self.tmp[ckey]["n_x"] = fp[f"{grp_name}/NCOLS"][()]
+        self.tmp[ckey]["n_y"] = fp[f"{grp_name}/NROWS"][()]
+        self.tmp[ckey]["s_x"] = fp[f"{grp_name}/SEPixelSizeX"][()]
+        self.tmp[ckey]["s_unit"] = "µm"  # TODO::always micron?
+        self.tmp[ckey]["s_y"] = fp[f"{grp_name}/SEPixelSizeY"][()]
+        # TODO::check that all data are consistent
+        # TODO::what is y and x depends on coordinate system
 
     def parse_and_normalize_group_ebsd_phases(self, fp, ckey: str):
-        grp_name = f"{self.prfx}/EBSD/Header"
+        grp_name = f"{self.prfx}/EBSD/Header/Phases"
+        if f"{grp_name}" not in fp:
+            raise ValueError(f"Unable parse {grp_name} !")
+
         # Phases, contains a subgroup for each phase where the name
         # of each subgroup is the index of the phase starting at 1.
-        if f"{grp_name}/Phases" in fp:
-            phase_ids = sorted(list(fp[f"{grp_name}/Phases"]), key=int)
-            self.tmp[ckey]["phase"] = []
-            self.tmp[ckey]["space_group"] = []
-            self.tmp[ckey]["phases"] = {}
-            for phase_id in phase_ids:
-                if phase_id.isdigit() is True:
-                    self.tmp[ckey]["phases"][int(phase_id)] = {}
-                    sub_grp_name = f"/{grp_name}/Phases/{phase_id}"
-                    # Name
-                    if f"{sub_grp_name}/Name" in fp:
-                        phase_name = read_strings_from_dataset(fp[f"{sub_grp_name}/Name"][()])
-                        self.tmp[ckey]["phases"][int(phase_id)]["phase_name"] = phase_name
-                    else:
-                        raise ValueError(f"Unable to parse {sub_grp_name}/Name !")
+        phase_ids = sorted(list(fp[f"{grp_name}"]), key=int)
+        self.tmp[ckey]["phase"] = []
+        self.tmp[ckey]["space_group"] = []
+        self.tmp[ckey]["phases"] = {}
+        for phase_id in phase_ids:
+            if phase_id.isdigit() is True:
+                self.tmp[ckey]["phases"][int(phase_id)] = {}
+                sub_grp_name = f"/{grp_name}/{phase_id}"
+                req_fields = ["Name", "LatticeConstants", "SpaceGroup"]
+                for req_field in req_fields:
+                    if f"{sub_grp_name}/{req_field}" not in fp:
+                        raise ValueError(f"Unable to parse {sub_grp_name}/{req_field} !")
+                # Name
+                phase_name = read_strings_from_dataset(fp[f"{sub_grp_name}/Name"][()])
+                self.tmp[ckey]["phases"][int(phase_id)]["phase_name"] = phase_name
 
-                    # Reference not available
-                    self.tmp[ckey]["phases"][int(phase_id)]["reference"] = "n/a"
+                # Reference not available
+                self.tmp[ckey]["phases"][int(phase_id)]["reference"] = "n/a"
 
-                    # LatticeConstants, a, b, c (angstrom) followed by alpha, beta and gamma angles in degree
-                    if f"{sub_grp_name}/LatticeConstants" in fp:
-                        values = np.asarray(fp[f"{sub_grp_name}/LatticeConstants"][:].flatten())
-                        a_b_c = values[0:3]
-                        angles = values[3:6]
-                        self.tmp[ckey]["phases"][int(phase_id)]["a_b_c"] \
-                            = a_b_c * 0.1
-                        self.tmp[ckey]["phases"][int(phase_id)]["alpha_beta_gamma"] \
-                            = angles
-                    else:
-                        raise ValueError(f"Unable to parse {sub_grp_name}/LatticeConstants !")
+                # LatticeConstants, a, b, c (angstrom) followed by alpha, beta and gamma angles in degree
+                values = np.asarray(fp[f"{sub_grp_name}/LatticeConstants"][:].flatten())
+                a_b_c = values[0:3]
+                angles = values[3:6]
+                self.tmp[ckey]["phases"][int(phase_id)]["a_b_c"] = a_b_c * 0.1
+                self.tmp[ckey]["phases"][int(phase_id)]["alpha_beta_gamma"] = angles
 
-                    # Space Group, no, H5T_NATIVE_INT32, (1, 1), Space group index.
-                    # The attribute Symbol contains the string representation, for example P m -3 m.
-                    if f"{sub_grp_name}/SpaceGroup" in fp:
-                        spc_grp  = read_strings_from_dataset(fp[f"{sub_grp_name}/SpaceGroup"][()])
-                        if spc_grp in BRUKER_MAP_SPACEGROUP.keys():
-                            space_group = BRUKER_MAP_SPACEGROUP[spc_grp]
-                            self.tmp[ckey]["phases"][int(phase_id)]["space_group"] = space_group
-                        else:
-                            raise ValueError(f"Unable to decode improperly formatted space group {spc_grp} !")
-                    else:
-                        raise ValueError(f"Unable to parse {sub_grp_name}/SpaceGroup !")
-                    # formatting is a nightmare F m#ovl3m for F m 3bar m...
-                    if len(self.tmp[ckey]["space_group"]) > 0:
-                        self.tmp[ckey]["space_group"].append(space_group)
-                    else:
-                        self.tmp[ckey]["space_group"] = [space_group]
+                # Space Group, no, H5T_NATIVE_INT32, (1, 1), Space group index.
+                # The attribute Symbol contains the string representation, for example P m -3 m.
+                spc_grp  = read_strings_from_dataset(fp[f"{sub_grp_name}/SpaceGroup"][()])
+                if spc_grp in EBSD_MAP_SPACEGROUP.keys():
+                    space_group = EBSD_MAP_SPACEGROUP[spc_grp]
+                    self.tmp[ckey]["phases"][int(phase_id)]["space_group"] = space_group
+                else:
+                    raise ValueError(f"Unable to decode improperly formatted space group {spc_grp} !")
 
-                    if len(self.tmp[ckey]["phase"]) > 0:
-                        self.tmp[ckey]["phase"].append(
-                            Structure(title=phase_name, atoms=None,
-                                      lattice=Lattice(a_b_c[0], a_b_c[1], a_b_c[2],
-                                      angles[0], angles[1], angles[2])))
-                    else:
-                        self.tmp[ckey]["phase"] \
-                            = [Structure(title=phase_name, atoms=None,
-                                         lattice=Lattice(a_b_c[0], a_b_c[1], a_b_c[2],
-                                         angles[0], angles[1], angles[2]))]
-        else:
-            raise ValueError(f"Unable to parse {grp_name}/Phases !")
+                # formatting is a nightmare F m#ovl3m for F m 3bar m...
+                if len(self.tmp[ckey]["space_group"]) > 0:
+                    self.tmp[ckey]["space_group"].append(space_group)
+                else:
+                    self.tmp[ckey]["space_group"] = [space_group]
+
+                if len(self.tmp[ckey]["phase"]) > 0:
+                    self.tmp[ckey]["phase"].append(
+                        Structure(title=phase_name, atoms=None,
+                                  lattice=Lattice(a_b_c[0], a_b_c[1], a_b_c[2],
+                                  angles[0], angles[1], angles[2])))
+                else:
+                    self.tmp[ckey]["phase"] \
+                        = [Structure(title=phase_name, atoms=None,
+                                     lattice=Lattice(a_b_c[0], a_b_c[1], a_b_c[2],
+                                     angles[0], angles[1], angles[2]))]
 
     def parse_and_normalize_group_ebsd_data(self, fp, ckey: str):
         # no official documentation yet from Bruker but seems inspired by H5EBSD
         grp_name = f"{self.prfx}/EBSD/Data"
-        print(f"Parsing {grp_name}")
-        # Euler, yes, H5T_NATIVE_FLOAT, (size, 3), Orientation of Crystal (CS2) to Sample-Surface (CS1).
-        n_pts = 0
-        if f"{grp_name}/phi1" in fp and f"{grp_name}/PHI" in fp and f"{grp_name}/phi2" in fp:
-            n_pts = (np.shape(fp[f"{grp_name}/phi1"][:])[0],
-                     np.shape(fp[f"{grp_name}/PHI"][:])[0],
-                     np.shape(fp[f"{grp_name}/phi2"][:])[0])
-            if all_equal(n_pts) is True and n_pts[0] > 0:
-                self.tmp[ckey]["euler"] = np.zeros((n_pts[0], 3), np.float32)
-                column_id = 0
-                for angle in ["phi1", "PHI", "phi2"]:
-                    self.tmp[ckey]["euler"][:, column_id] \
-                        = np.asarray(fp[f"{grp_name}/{angle}"][:], np.float32)
-                    column_id += 1
-                is_degrees = False
-                is_negative = False
-                for column_id in [0, 1, 2]:
-                    if np.max(np.abs(self.tmp[ckey]["euler"][:, column_id])) > 2. * np.pi:
-                        is_degrees = True
-                    if np.min(self.tmp[ckey]["euler"][:, column_id]) < 0.:
-                        is_negative = True
-                if is_degrees is True:
-                    self.tmp[ckey]["euler"] = self.tmp[ckey]["euler"] / 180. * np.pi
-                if is_negative is True:
-                    symmetrize = [2. * np.pi, np.pi, 2. * np.pi]
-                    # TODO::symmetry in Euler space really at PHI=180deg?
-                    for column_id in [0, 1, 2]:
-                        self.tmp[ckey]["euler"][:, column_id] \
-                            = self.tmp[ckey]["euler"][:, column_id] + symmetrize[column_id]
-                n_pts = n_pts[0]
-            # inconsistency f32 in file although specification states float
-                #Rotation.from_euler(euler=fp[f"{grp_name}/Euler"],
-                 #                                 direction='lab2crystal',
-                  #                                degrees=is_degrees)
-        else:
-            raise ValueError(f"Unable to parse {grp_name}/phi1, ../PHI, ../phi2 !")
+        if f"{grp_name}" not in fp:
+            raise ValueError(f"Unable to parse {grp_name} !")
+
+        req_fields = ["phi1", "PHI", "phi2", "Phase", "X SAMPLE", "Y SAMPLE", "MAD"]
+        for req_field in req_fields:
+            if f"{grp_name}/{req_field}" not in fp:
+                raise ValueError(f"Unable to parse {grp_name}/{req_field} !")
+
+        # Euler
+        n_pts = (np.shape(fp[f"{grp_name}/phi1"][:])[0],
+                 np.shape(fp[f"{grp_name}/PHI"][:])[0],
+                 np.shape(fp[f"{grp_name}/phi2"][:])[0])
+        if all_equal(n_pts) is True and n_pts[0] == (self.tmp[ckey]["n_x"] * self.tmp[ckey]["n_y"]):
+            self.tmp[ckey]["euler"] = np.zeros((n_pts[0], 3), np.float32)
+            column_id = 0
+            for angle in ["phi1", "PHI", "phi2"]:
+                self.tmp[ckey]["euler"][:, column_id] \
+                    = np.asarray(fp[f"{grp_name}/{angle}"][:], np.float32)
+                column_id += 1
+            self.tmp[ckey]["euler"] = format_euler_parameterization(self.tmp[ckey]["euler"])
+            n_pts = n_pts[0]
+        # inconsistency f32 in file although specification states float
+        # Rotation.from_euler(euler=fp[f"{grp_name}/Euler"],
+        #                                 direction='lab2crystal',
+        #                                degrees=is_degrees)
 
         # index of phase, 0 if not indexed
-        # # no normalization needed, also in NXem_ebsd the null model notIndexed is phase_identifier 0
-        if f"{grp_name}/Phase" in fp:
-            if np.shape(fp[f"{grp_name}/Phase"][:])[0] == n_pts:
-                self.tmp[ckey]["phase_id"] = np.asarray(fp[f"{grp_name}/Phase"][:], np.int32)
-            else:
-                raise ValueError(f"{grp_name}/Phase has unexpected shape !")
+        # no normalization needed, also in NXem_ebsd the null model notIndexed is phase_identifier 0
+        if np.shape(fp[f"{grp_name}/Phase"][:])[0] == n_pts:
+            self.tmp[ckey]["phase_id"] = np.asarray(fp[f"{grp_name}/Phase"][:], np.int32)
         else:
-            raise ValueError(f"Unable to parse {grp_name}/Phase !")
+            raise ValueError(f"{grp_name}/Phase has unexpected shape !")
 
         # X
-        if f"{grp_name}/X SAMPLE" in fp:
-            if np.shape(fp[f"{grp_name}/X SAMPLE"][:])[0] == n_pts:
-                self.tmp[ckey]["scan_point_x"] \
-                    = np.asarray(fp[f"{grp_name}/X SAMPLE"][:], np.float32)
-            else:
-                raise ValueError(f"{grp_name}/X SAMPLE has unexpected shape !")
+        if np.shape(fp[f"{grp_name}/X SAMPLE"][:])[0] == n_pts:
+            self.tmp[ckey]["scan_point_x"] \
+                = np.asarray(fp[f"{grp_name}/X SAMPLE"][:], np.float32)
         else:
-            raise ValueError(f"Unable to parse {grp_name}/X SAMPLE !")
+            raise ValueError(f"{grp_name}/X SAMPLE has unexpected shape !")
 
         # Y
-        if f"{grp_name}/Y SAMPLE" in fp:
-            if np.shape(fp[f"{grp_name}/Y SAMPLE"][:])[0] == n_pts:
-                self.tmp[ckey]["scan_point_y"] \
-                    = np.asarray(fp[f"{grp_name}/Y SAMPLE"], np.float32)
-            else:
-                raise ValueError(f"{grp_name}/Y SAMPLE has unexpected shape !")
+        if np.shape(fp[f"{grp_name}/Y SAMPLE"][:])[0] == n_pts:
+            self.tmp[ckey]["scan_point_y"] \
+                = np.asarray(fp[f"{grp_name}/Y SAMPLE"], np.float32)
         else:
-            raise ValueError(f"Unable to parse {grp_name}/Y SAMPLE !")
+            raise ValueError(f"{grp_name}/Y SAMPLE has unexpected shape !")
 
         # Band Contrast is not stored in Bruker but Radon Quality or MAD
         # but this is s.th. different as it is the mean angular deviation between
         # indexed with simulated and measured pattern
-        if f"{grp_name}/MAD" in fp:
-            if np.shape(fp[f"{grp_name}/MAD"][:])[0] == n_pts:
-                self.tmp[ckey]["mad"] = np.asarray(fp[f"{grp_name}/MAD"][:], np.float32)
-            else:
-                raise ValueError(f"{grp_name}/MAD has unexpected shape !")
+        if np.shape(fp[f"{grp_name}/MAD"][:])[0] == n_pts:
+            self.tmp[ckey]["mad"] = np.asarray(fp[f"{grp_name}/MAD"][:], np.float32)
         else:
-            raise ValueError(f"Unable to parse {grp_name}/MAD !")
-
+            raise ValueError(f"{grp_name}/MAD has unexpected shape !")

pynxtools/dataconverter/readers/em/subparsers/hfive_edax.py
@@ -17,10 +17,27 @@
 #
 """(Sub-)parser mapping concepts and content from EDAX/AMETEK *.oh5/*.h5 (OIM Analysis) files on NXem."""
 
+import os
+from typing import Dict, Any, List
 import numpy as np
 import h5py
+from itertools import groupby
+# import imageio.v3 as iio
+from PIL import Image as pil
+
+import diffsims
+import orix
+from diffpy.structure import Lattice, Structure
+from orix import plot
+from orix.crystal_map import create_coordinate_arrays, CrystalMap, PhaseList
+from orix.quaternion import Rotation
+from orix.vector import Vector3d
+
+import matplotlib.pyplot as plt
+
 from pynxtools.dataconverter.readers.em.subparsers.hfive_base import HdfFiveBaseParser
-from pynxtools.dataconverter.readers.em.utils.hfive_utils import read_strings_from_dataset
+from pynxtools.dataconverter.readers.em.utils.hfive_utils import \
+    read_strings_from_dataset, format_euler_parameterization
 
 
 class HdfFiveEdaxOimAnalysisReader(HdfFiveBaseParser):
@@ -47,25 +64,153 @@ class HdfFiveEdaxOimAnalysisReader(HdfFiveBaseParser):
 
     def check_if_supported(self):
         """Check if instance matches all constraints to qualify as old EDAX"""
-        self.supported = False
+        self.supported = 0  # voting-based
         with h5py.File(self.file_path, "r") as h5r:
-            if "/Manufacturer" in h5r:
-                self.version["tech_partner"] \
-                    = super().read_strings_from_dataset(h5r["/Manufacturer"][()])
-                # for 8.6.0050 but for 8.5.1002 it is a matrix, this is because how strings end up in HDF5 allowed for so much flexibility!
-                if self.version["tech_partner"] not in self.supported_version["tech_partner"]:
+            req_fields = ["Manufacturer", "Version"]
+            for req_field in req_fields:
+                if f"/{req_field}" not in h5r:
                     self.supported = False
-            else:
-                self.supported = False
-            if "/Version" in h5r:
-                self.version["schema_version"] \
-                    = super().read_strings_from_dataset(h5r["/Version"][()])
-                if self.version["schema_version"] not in self.supported_version["schema_version"]:
-                    self.supported = False
-            else:
-                self.supported = False
+                    return
 
-            if self.supported is True:
+            self.version["tech_partner"] = read_strings_from_dataset(h5r["/Manufacturer"][()])
+            # for 8.6.0050 but for 8.5.1002 it is a matrix, this is because how strings end up in HDF5 allowed for so much flexibility!
+            if self.version["tech_partner"] in self.supported_version["tech_partner"]:
+                self.supported += 1
+            self.version["schema_version"] = read_strings_from_dataset(h5r["/Version"][()])
+            if self.version["schema_version"] in self.supported_version["schema_version"]:
+                self.supported += 1
+
+            if self.supported == 2:
                 self.version["schema_name"] = self.supported_version["schema_name"]
                 self.version["writer_name"] = self.supported_version["writer_name"]
                 self.version["writer_version"] = self.supported_version["writer_version"]
+                self.supported = True
+            else:
+                self.supported = False
+
+    def parse_and_normalize(self):
+        """Read and normalize away EDAX-specific formatting with an equivalent in NXem."""
+        with h5py.File(f"{self.file_path}", "r") as h5r:
+            cache_id = 0
+            grp_names = list(h5r["/"])
+            for grp_name in grp_names:
+                if grp_name not in ["Version", "Manufacturer"]:
+                    self.prfx = f"/{grp_name}"
+                    ckey = self.init_named_cache(f"ebsd{cache_id}")
+                    self.parse_and_normalize_group_ebsd_header(h5r, ckey)
+                    self.parse_and_normalize_group_ebsd_phases(h5r, ckey)
+                    self.parse_and_normalize_group_ebsd_data(h5r, ckey)
+                    # add more information to pass to hfive parser
+                    cache_id += 1
+
+    def parse_and_normalize_group_ebsd_header(self, fp, ckey: str):
+        grp_name = f"{self.prfx}/EBSD/Header"
+        if f"{grp_name}" not in fp:
+            raise ValueError(f"Unable to parse {grp_name} !")
+
+        grid_type = None
+        n_pts = 0
+        req_fields = ["Grid Type", "Step X", "Step Y", "nColumns", "nRows"]
+        for req_field in req_fields:
+            if f"{grp_name}/{req_field}" not in fp:
+                raise ValueError(f"Unable to parse {grp_name}/{req_field} !")
+
+        grid_type = read_strings_from_dataset(fp[f"{grp_name}/Grid Type"][()])
+        if grid_type not in ["HexGrid", "SqrGrid"]:
+            raise ValueError(f"Grid Type {grid_type} is currently not supported !")
+        self.tmp[ckey]["grid_type"] = grid_type
+        self.tmp[ckey]["s_x"] = fp[f"{grp_name}/Step X"][()]
+        self.tmp[ckey]["s_unit"] = "µm"  # TODO::always micron?
+        self.tmp[ckey]["n_x"] = fp[f"{grp_name}/nColumns"][()]
+        self.tmp[ckey]["s_y"] = fp[f"{grp_name}/Step Y"][()]
+        self.tmp[ckey]["n_y"] = fp[f"{grp_name}/nRows"][()]
+        # TODO::check that all data are consistent
+
+    def parse_and_normalize_group_ebsd_phases(self, fp, ckey: str):
+        grp_name = f"{self.prfx}/EBSD/Header/Phase"
+        # Phases, contains a subgroup for each phase where the name
+        # of each subgroup is the index of the phase starting at 1.
+        if f"{grp_name}" in fp:
+            phase_ids = sorted(list(fp[f"{grp_name}"]), key=int)
+            self.tmp[ckey]["phase"] = []
+            self.tmp[ckey]["space_group"] = []
+            self.tmp[ckey]["phases"] = {}
+            for phase_id in phase_ids:
+                if phase_id.isdigit() is True:
+                    self.tmp[ckey]["phases"][int(phase_id)] = {}
+                    sub_grp_name = f"{grp_name}/{phase_id}"
+                    # Name
+                    if f"{sub_grp_name}/MaterialName" in fp:
+                        phase_name = read_strings_from_dataset(fp[f"{sub_grp_name}/MaterialName"][0])
+                        self.tmp[ckey]["phases"][int(phase_id)]["phase_name"] = phase_name
+                    else:
+                        raise ValueError(f"Unable to parse {sub_grp_name}/MaterialName !")
+
+                    # Reference not available only Info but this can be empty
+                    self.tmp[ckey]["phases"][int(phase_id)]["reference"] = "n/a"
+
+                    req_fields = ["a", "b", "c", "alpha", "beta", "gamma"]
+                    for req_field in req_fields:
+                        if f"{sub_grp_name}/Lattice Constant {req_field}" not in fp:
+                            raise ValueError(f"Unable to parse ../Lattice Constant {req_field} !")
+                    a_b_c = [fp[f"{sub_grp_name}/Lattice Constant a"][()],
+                             fp[f"{sub_grp_name}/Lattice Constant b"][()],
+                             fp[f"{sub_grp_name}/Lattice Constant c"][()]]
+                    angles = [fp[f"{sub_grp_name}/Lattice Constant alpha"][()],
+                              fp[f"{sub_grp_name}/Lattice Constant beta"][()],
+                              fp[f"{sub_grp_name}/Lattice Constant gamma"][()]]
+                    self.tmp[ckey]["phases"][int(phase_id)]["a_b_c"] \
+                        = np.asarray(a_b_c, np.float32) * 0.1
+                    self.tmp[ckey]["phases"][int(phase_id)]["alpha_beta_gamma"] \
+                        = np.asarray(angles, np.float32)
+
+                    # Space Group not stored, only laue group, point group and symmetry
+                    # problematic because mapping is not bijective!
+                    # if you know the space group we know laue and point group and symmetry
+                    # but the opposite direction leaves room for ambiguities
+                    space_group = "n/a"
+                    self.tmp[ckey]["phases"][int(phase_id)]["space_group"] = space_group
+
+                    if len(self.tmp[ckey]["space_group"]) > 0:
+                        self.tmp[ckey]["space_group"].append(space_group)
+                    else:
+                        self.tmp[ckey]["space_group"] = [space_group]
+
+                    if len(self.tmp[ckey]["phase"]) > 0:
+                        self.tmp[ckey]["phase"].append(
+                            Structure(title=phase_name, atoms=None,
+                                      lattice=Lattice(a_b_c[0], a_b_c[1], a_b_c[2],
+                                      angles[0], angles[1], angles[2])))
+                    else:
+                        self.tmp[ckey]["phase"] \
+                            = [Structure(title=phase_name, atoms=None,
+                                         lattice=Lattice(a_b_c[0], a_b_c[1], a_b_c[2],
+                                         angles[0], angles[1], angles[2]))]
+        else:
+            raise ValueError(f"Unable to parse {grp_name} !")
+
+    def parse_and_normalize_group_ebsd_data(self, fp, ckey: str):
+        grp_name = f"{self.prfx}/EBSD/Data"
+        if f"{grp_name}" not in fp:
+            raise ValueError(f"Unable to parse {grp_name} !")
+
+        req_fields = ["CI", "Phase", "Phi1", "Phi", "Phi2", "X Position", "Y Position"]
+        for req_field in req_fields:
+            if f"{grp_name}/{req_field}" not in fp:
+                raise ValueError(f"Unable to parse {grp_name}/{req_field} !")
+
+        n_pts = self.tmp[ckey]["n_x"] * self.tmp[ckey]["n_y"]
+        self.tmp[ckey]["euler"] = np.zeros((n_pts, 3), np.float32)
+        self.tmp[ckey]["euler"][:, 0] = np.asarray(fp[f"{grp_name}/Phi1"][:], np.float32)
+        self.tmp[ckey]["euler"][:, 1] = np.asarray(fp[f"{grp_name}/Phi"][:], np.float32)
+        self.tmp[ckey]["euler"][:, 2] = np.asarray(fp[f"{grp_name}/Phi2"][:], np.float32)
+        # TODO::seems to be the situation in the example but there is no documentation
+        self.tmp[ckey]["euler"] = format_euler_parameterization(self.tmp[ckey]["euler"])
+
+        self.tmp[ckey]["phase_id"] = np.asarray(fp[f"{grp_name}/Phase"][:], np.int32)
+        # promoting int8 to int32 no problem
+        self.tmp[ckey]["ci"] = np.asarray(fp[f"{grp_name}/CI"][:], np.float32)
+        self.tmp[ckey]["scan_point_x"] = np.asarray(
+                fp[f"{grp_name}/X Position"][:] * self.tmp[ckey]["s_x"] + 0., np.float32)
+        self.tmp[ckey]["scan_point_y"] = np.asarray(
+                fp[f"{grp_name}/Y Position"][:] * self.tmp[ckey]["s_y"] + 0., np.float32)

pynxtools/dataconverter/readers/em/subparsers/hfive_oxford.py
@@ -67,37 +67,34 @@ class HdfFiveOxfordReader(HdfFiveBaseParser):
 
     def check_if_supported(self):
         """Check if instance matches all constraints to qualify as supported H5OINA"""
-        self.supported = True  # try to falsify
+        self.supported = 0  # voting-based
         with h5py.File(self.file_path, "r") as h5r:
-            if "/Manufacturer" in h5r:
-                self.version["tech_partner"] \
-                    = read_strings_from_dataset(h5r["/Manufacturer"][()])
-                if self.version["tech_partner"] not in self.supported_version["tech_partner"]:
-                    # print(f"{self.version['tech_partner']} is not {self.version['tech_partner']} !")
+            req_fields = ["Manufacturer", "Software Version", "Format Version"]
+            for req_field in req_fields:
+                if f"/{req_field}" not in h5r:
                     self.supported = False
-            else:
-                self.supported = False
+                    return
+
+            self.version["tech_partner"] = read_strings_from_dataset(h5r["/Manufacturer"][()])
+            if self.version["tech_partner"] in self.supported_version["tech_partner"]:
+                # print(f"{self.version['tech_partner']} is not {self.version['tech_partner']} !")
+                self.supported += 1
             # only because we know (thanks to Philippe Pinard who wrote the H5OINA writer) that different
             # writer versions should implement the different HDF version correctly we can lift the
             # constraint on the writer_version for which we had examples available
-            if "/Software Version" in h5r:
-                self.version["writer_version"] \
-                    = read_strings_from_dataset(h5r["/Software Version"][()])
-                if self.version["writer_version"] not in self.supported_version["writer_version"]:
-                    self.supported = False
-            else:
-                self.supported = False
-            if "/Format Version" in h5r:
-                self.version["schema_version"] \
-                    = read_strings_from_dataset(h5r["/Format Version"][()])
-                if self.version["schema_version"] not in self.supported_version["schema_version"]:
-                    self.supported = False
-            else:
-                self.supported = False
+            self.version["writer_version"] = read_strings_from_dataset(h5r["/Software Version"][()])
+            if self.version["writer_version"] in self.supported_version["writer_version"]:
+                self.supported += 1
+            self.version["schema_version"] = read_strings_from_dataset(h5r["/Format Version"][()])
+            if self.version["schema_version"] in self.supported_version["schema_version"]:
+                self.supported += 1
 
-        if self.supported is True:
-            self.version["schema_name"] = self.supported_version["schema_name"]
-            self.version["writer_name"] = self.supported_version["writer_name"]
+            if self.supported == 3:
+                self.version["schema_name"] = self.supported_version["schema_name"]
+                self.version["writer_name"] = self.supported_version["writer_name"]
+                self.supported = True
+            else:
+                self.supported = False
 
     def parse_and_normalize(self):
         """Read and normalize away Oxford-specific formatting with an equivalent in NXem."""
@@ -116,152 +113,134 @@ class HdfFiveOxfordReader(HdfFiveBaseParser):
                     cache_id += 1
 
     def parse_and_normalize_slice_ebsd_header(self, fp, ckey: str):
-        # X Cells, yes, H5T_NATIVE_INT32, (1, 1), Map: Width in pixels, Line scan: Length in pixels.
         grp_name = f"{self.prfx}/EBSD/Header"
-        if f"{grp_name}/X Cells" in fp:
-            self.tmp[ckey]["n_x"] = fp[f"{grp_name}/X Cells"][0]
-        else:
-            raise ValueError(f"Unable to parse {grp_name}/X Cells !")
+        if f"{grp_name}" not in fp:
+            raise ValueError(f"Unable to parse {grp_name} !")
+
+        req_fields = ["X Cells", "Y Cells", "X Step", "Y Step"]
+        for req_field in req_fields:
+            if f"{grp_name}/{req_field}" not in fp:
+                raise ValueError(f"Unable to parse {grp_name}/{req_field} !")
+
+        # X Cells, yes, H5T_NATIVE_INT32, (1, 1), Map: Width in pixels, Line scan: Length in pixels.
+        self.tmp[ckey]["n_x"] = fp[f"{grp_name}/X Cells"][0]
         # Y Cells, yes, H5T_NATIVE_INT32, (1, 1), Map: Height in pixels. Line scan: Always set to 1.
-        if f"{grp_name}/Y Cells" in fp:
-            self.tmp[ckey]["n_y"] = fp[f"{grp_name}/Y Cells"][0]
-        else:
-            raise ValueError(f"Unable to parse{grp_name}/Y Cells !")
+        self.tmp[ckey]["n_y"] = fp[f"{grp_name}/Y Cells"][0]
         # X Step, yes, H5T_NATIVE_FLOAT, (1, 1), Map: Step size along x-axis in micrometers. Line scan: step size along the line scan in micrometers.
-        if f"{grp_name}/X Step" in fp:
-            if read_strings_from_dataset(fp[f"{grp_name}/X Step"].attrs["Unit"]) == "um":
-                self.tmp[ckey]["s_x"] = fp[f"{grp_name}/X Step"][0]
-                self.tmp[ckey]["s_unit"] = "µm"
-            else:
-                raise ValueError(f"Unexpected X Step Unit attribute !")
+        if read_strings_from_dataset(fp[f"{grp_name}/X Step"].attrs["Unit"]) == "um":
+            self.tmp[ckey]["s_x"] = fp[f"{grp_name}/X Step"][0]
+            self.tmp[ckey]["s_unit"] = "µm"
         else:
-            raise ValueError(f"Unable to parse {grp_name}/X Step !")
+            raise ValueError(f"Unexpected X Step Unit attribute !")
         # Y Step, yes, H5T_NATIVE_FLOAT, (1, 1), Map: Step size along y-axis in micrometers. Line scan: Always set to 0.
-        if f"{grp_name}/Y Step" in fp:
-            if read_strings_from_dataset(fp[f"{grp_name}/Y Step"].attrs["Unit"]) == "um":
-                self.tmp[ckey]["s_y"] = fp[f"{grp_name}/Y Step"][0]
-            else:
-                raise ValueError(f"Unexpected Y Step Unit attribute !")
+        if read_strings_from_dataset(fp[f"{grp_name}/Y Step"].attrs["Unit"]) == "um":
+            self.tmp[ckey]["s_y"] = fp[f"{grp_name}/Y Step"][0]
         else:
-            raise ValueError(f"Unable to parse {grp_name}/Y Step !")
+            raise ValueError(f"Unexpected Y Step Unit attribute !")
         # TODO::check that all data in the self.oina are consistent
 
     def parse_and_normalize_slice_ebsd_phases(self, fp, ckey: str):
         """Parse EBSD header section for specific slice."""
-        grp_name = f"{self.prfx}/EBSD/Header"
+        grp_name = f"{self.prfx}/EBSD/Header/Phases"
+        if f"{grp_name}" not in fp:
+            raise ValueError(f"Unable to parse {grp_name} !")
+
         # Phases, yes, Contains a subgroup for each phase where the name
         # of each subgroup is the index of the phase starting at 1.
-        if f"{grp_name}/Phases" in fp:
-            phase_ids = sorted(list(fp[f"{grp_name}/Phases"]), key=int)
-            self.tmp[ckey]["phase"] = []
-            self.tmp[ckey]["space_group"] = []
-            self.tmp[ckey]["phases"] = {}
-            for phase_id in phase_ids:
-                if phase_id.isdigit() is True:
-                    self.tmp[ckey]["phases"][int(phase_id)] = {}
-                    sub_grp_name = f"/{grp_name}/Phases/{phase_id}"
-                    # Phase Name, yes, H5T_STRING, (1, 1)
-                    if f"{sub_grp_name}/Phase Name" in fp:
-                        phase_name = read_strings_from_dataset(fp[f"{sub_grp_name}/Phase Name"][()])
-                        self.tmp[ckey]["phases"][int(phase_id)]["phase_name"] = phase_name
-                    else:
-                        raise ValueError(f"Unable to parse {sub_grp_name}/Phase Name !")
+        phase_ids = sorted(list(fp[f"{grp_name}"]), key=int)
+        self.tmp[ckey]["phase"] = []
+        self.tmp[ckey]["space_group"] = []
+        self.tmp[ckey]["phases"] = {}
+        for phase_id in phase_ids:
+            if phase_id.isdigit() is True:
+                self.tmp[ckey]["phases"][int(phase_id)] = {}
+                sub_grp_name = f"/{grp_name}/{phase_id}"
 
-                    # Reference, yes, H5T_STRING, (1, 1), Changed in version 2.0 to mandatory
-                    if f"{sub_grp_name}/Reference" in fp:
-                        self.tmp[ckey]["phases"][int(phase_id)]["reference"] \
-                            = read_strings_from_dataset(fp[f"{sub_grp_name}/Reference"][()])
-                    else:
-                        raise ValueError(f"Unable to parse {sub_grp_name}/Reference !")
+                req_fields = ["Phase Name", "Reference", "Lattice Angles",
+                              "Lattice Dimensions", "Space Group"]
+                for req_field in req_fields:
+                    if f"{sub_grp_name}/{req_field}" not in fp:
+                        raise ValueError(f"Unable to parse {sub_grp_name}/{req_field} !")
 
-                    # Lattice Angles, yes, H5T_NATIVE_FLOAT, (1, 3), Three columns for the alpha, beta and gamma angles in radians
-                    if f"{sub_grp_name}/Lattice Angles" in fp:
-                        is_degrees = False
-                        if read_strings_from_dataset(fp[f"{sub_grp_name}/Lattice Angles"].attrs["Unit"]) == "rad":
-                            is_degrees = False
-                        angles = np.asarray(fp[f"{sub_grp_name}/Lattice Angles"][:].flatten()) / np.pi * 180.
-                        self.tmp[ckey]["phases"][int(phase_id)]["alpha_beta_gamma"] \
-                            = angles
-                    else:
-                        raise ValueError(f"Unable to parse {sub_grp_name}/Lattice Angles !")
+                # Phase Name, yes, H5T_STRING, (1, 1)
+                phase_name = read_strings_from_dataset(fp[f"{sub_grp_name}/Phase Name"][()])
+                self.tmp[ckey]["phases"][int(phase_id)]["phase_name"] = phase_name
 
-                    # Lattice Dimensions, yes, H5T_NATIVE_FLOAT, (1, 3), Three columns for a, b and c dimensions in Angstroms
-                    if f"{sub_grp_name}/Lattice Dimensions" in fp:
-                        is_nanometer = False
-                        if read_strings_from_dataset(fp[f"{sub_grp_name}/Lattice Dimensions"].attrs["Unit"]) == "angstrom":
-                            is_nanometer = False
-                        a_b_c = np.asarray(fp[f"{sub_grp_name}/Lattice Dimensions"][:].flatten()) * 0.1
-                        self.tmp[ckey]["phases"][int(phase_id)]["a_b_c"] = a_b_c
-                    else:
-                        raise ValueError(f"Unable to parse {sub_grp_name}/Lattice Dimensions !")
+                # Reference, yes, H5T_STRING, (1, 1), Changed in version 2.0 to mandatory
+                self.tmp[ckey]["phases"][int(phase_id)]["reference"] \
+                    = read_strings_from_dataset(fp[f"{sub_grp_name}/Reference"][()])
 
-                    # Space Group, no, H5T_NATIVE_INT32, (1, 1), Space group index.
-                    # The attribute Symbol contains the string representation, for example P m -3 m.
-                    if f"{sub_grp_name}/Space Group" in fp:
-                        space_group = int(fp[f"{sub_grp_name}/Space Group"][0])
-                        self.tmp[ckey]["phases"][int(phase_id)]["space_group"] = space_group
-                    else:
-                        raise ValueError(f"Unable to parse {sub_grp_name}/Space Group !")
-                    if len(self.tmp[ckey]["space_group"]) > 0:
-                        self.tmp[ckey]["space_group"].append(space_group)
-                    else:
-                        self.tmp[ckey]["space_group"] = [space_group]
+                # Lattice Angles, yes, H5T_NATIVE_FLOAT, (1, 3), Three columns for the alpha, beta and gamma angles in radians
+                is_degrees = False
+                if read_strings_from_dataset(fp[f"{sub_grp_name}/Lattice Angles"].attrs["Unit"]) == "rad":
+                    is_degrees = False
+                angles = np.asarray(fp[f"{sub_grp_name}/Lattice Angles"][:].flatten()) / np.pi * 180.
+                self.tmp[ckey]["phases"][int(phase_id)]["alpha_beta_gamma"] = angles
 
-                    if len(self.tmp[ckey]["phase"]) > 0:
-                        self.tmp[ckey]["phase"].append(
-                            Structure(title=phase_name, atoms=None,
-                                      lattice=Lattice(a_b_c[0], a_b_c[1], a_b_c[2],
-                                      angles[0], angles[1], angles[2])))
-                    else:
-                        self.tmp[ckey]["phase"] \
-                            = [Structure(title=phase_name, atoms=None,
-                                         lattice=Lattice(a_b_c[0], a_b_c[1], a_b_c[2],
-                                         angles[0], angles[1], angles[2]))]
-        else:
-            raise ValueError(f"Unable to parse {grp_name}/Phases !")
+                # Lattice Dimensions, yes, H5T_NATIVE_FLOAT, (1, 3), Three columns for a, b and c dimensions in Angstroms
+                is_nanometer = False
+                if read_strings_from_dataset(fp[f"{sub_grp_name}/Lattice Dimensions"].attrs["Unit"]) == "angstrom":
+                    is_nanometer = False
+                a_b_c = np.asarray(fp[f"{sub_grp_name}/Lattice Dimensions"][:].flatten()) * 0.1
+                self.tmp[ckey]["phases"][int(phase_id)]["a_b_c"] = a_b_c
+
+                # Space Group, no, H5T_NATIVE_INT32, (1, 1), Space group index.
+                # The attribute Symbol contains the string representation, for example P m -3 m.
+                space_group = int(fp[f"{sub_grp_name}/Space Group"][0])
+                self.tmp[ckey]["phases"][int(phase_id)]["space_group"] = space_group
+                if len(self.tmp[ckey]["space_group"]) > 0:
+                    self.tmp[ckey]["space_group"].append(space_group)
+                else:
+                    self.tmp[ckey]["space_group"] = [space_group]
+
+                if len(self.tmp[ckey]["phase"]) > 0:
+                    self.tmp[ckey]["phase"].append(
+                        Structure(title=phase_name, atoms=None,
+                                    lattice=Lattice(a_b_c[0], a_b_c[1], a_b_c[2],
+                                    angles[0], angles[1], angles[2])))
+                else:
+                    self.tmp[ckey]["phase"] \
+                        = [Structure(title=phase_name, atoms=None,
+                                        lattice=Lattice(a_b_c[0], a_b_c[1], a_b_c[2],
+                                        angles[0], angles[1], angles[2]))]
 
     def parse_and_normalize_slice_ebsd_data(self, fp, ckey: str):
         # https://github.com/oinanoanalysis/h5oina/blob/master/H5OINAFile.md
         grp_name = f"{self.prfx}/EBSD/Data"
-        print(f"Parsing {grp_name}")
+        if f"{grp_name}" not in fp:
+            raise ValueError(f"Unable to parse {grp_name} !")
+
+        req_fields = ["Euler", "Phase", "X", "Y", "Band Contrast"]
+        for req_field in req_fields:
+            if f"{grp_name}/{req_field}" not in fp:
+                raise ValueError(f"Unable to parse {grp_name}/{req_field} !")
+
         # Euler, yes, H5T_NATIVE_FLOAT, (size, 3), Orientation of Crystal (CS2) to Sample-Surface (CS1).
-        if f"{grp_name}/Euler" in fp:
+        is_degrees = False
+        is_negative = False
+        if read_strings_from_dataset(fp[f"{grp_name}/Euler"].attrs["Unit"]) == "rad":
             is_degrees = False
-            if read_strings_from_dataset(fp[f"{grp_name}/Euler"].attrs["Unit"]) == "rad":
-                is_degrees = False
-            self.tmp[ckey]["euler"] = np.asarray(fp[f"{grp_name}/Euler"], np.float32)
-            # inconsistency f32 in file although specification states float
-                #Rotation.from_euler(euler=fp[f"{grp_name}/Euler"],
-                 #                                 direction='lab2crystal',
-                  #                                degrees=is_degrees)
-        else:
-            raise ValueError(f"Unable to parse {grp_name}/Euler !")
+        self.tmp[ckey]["euler"] = np.asarray(fp[f"{grp_name}/Euler"], np.float32)
+        # TODO::handle possible case of negative Euler angles (examples though do not indicate)
+        # that AZTec reports negative Euler angles...
+        # inconsistency f32 in file although specification states float
+        # Rotation.from_euler(euler=fp[f"{grp_name}/Euler"],
+        #                                 direction='lab2crystal',
+        #                                degrees=is_degrees)
 
         # Phase, yes, H5T_NATIVE_INT32, (size, 1), Index of phase, 0 if not indexed
         # no normalization needed, also in NXem_ebsd the null model notIndexed is phase_identifier 0
-        if f"{grp_name}/Phase" in fp:
-            self.tmp[ckey]["phase_id"] = np.asarray(fp[f"{grp_name}/Phase"], np.int32)
-        else:
-            raise ValueError(f"Unable to parse {grp_name}/Phase !")
+        self.tmp[ckey]["phase_id"] = np.asarray(fp[f"{grp_name}/Phase"], np.int32)
 
         # X, no, H5T_NATIVE_FLOAT, (size, 1), X position of each pixel in micrometers (origin: top left corner)
-        if f"{grp_name}/X" in fp:
-            self.tmp[ckey]["scan_point_x"] = np.asarray(fp[f"{grp_name}/X"], np.float32)
-            # inconsistency f32 in file although specification states float
-        else:
-            raise ValueError(f"Unable to parse {grp_name}/X !")
+        self.tmp[ckey]["scan_point_x"] = np.asarray(fp[f"{grp_name}/X"], np.float32)
+        # inconsistency f32 in file although specification states float
 
         # Y, no, H5T_NATIVE_FLOAT, (size, 1), Y position of each pixel in micrometers (origin: top left corner)
-        if f"{grp_name}/Y" in fp:
-            self.tmp[ckey]["scan_point_y"] = np.asarray(fp[f"{grp_name}/Y"], np.float32)
-            # inconsistency f32 in file although specification states float
-        else:
-            raise ValueError(f"Unable to parse {grp_name}/Y !")
+        self.tmp[ckey]["scan_point_y"] = np.asarray(fp[f"{grp_name}/Y"], np.float32)
+        # inconsistency f32 in file although specification states float
 
         # Band Contrast, no, H5T_NATIVE_INT32, (size, 1)
-        if f"{grp_name}/Band Contrast" in fp:
-            self.tmp[ckey]["band_contrast"] = np.asarray(fp[f"{grp_name}/Band Contrast"], np.int32)
-            # inconsistency uint8 in file although specification states should be int32
-            # promoting uint8 to int32 no problem
-        else:
-            raise ValueError(f"Unable to parse {grp_name}/Band Contrast !")
+        self.tmp[ckey]["band_contrast"] = np.asarray(fp[f"{grp_name}/Band Contrast"], np.int32)
+        # inconsistency uint8 in file although specification states should be int32
+        # promoting uint8 to int32 no problem

pynxtools/dataconverter/readers/em/subparsers/nxs_hfive.py
@@ -55,10 +55,10 @@ from pynxtools.dataconverter.readers.em.utils.hfive_web_constants import HFIVE_W
 
 from pynxtools.dataconverter.readers.em.subparsers.hfive_oxford import HdfFiveOxfordReader
 from pynxtools.dataconverter.readers.em.subparsers.hfive_bruker import HdfFiveBrukerEspritReader
-# from pynxtools.dataconverter.readers.em.subparsers.hfive_edax import HdfFiveEdaxOimAnalysisReader
+from pynxtools.dataconverter.readers.em.subparsers.hfive_edax import HdfFiveEdaxOimAnalysisReader
 from pynxtools.dataconverter.readers.em.subparsers.hfive_apex import HdfFiveEdaxApexReader
+from pynxtools.dataconverter.readers.em.subparsers.hfive_ebsd import HdfFiveCommunityReader
 # from pynxtools.dataconverter.readers.em.subparsers.hfive_emsoft import HdfFiveEmSoftReader
-# from pynxtools.dataconverter.readers.em.subparsers.hfive_ebsd import HdfFiveCommunityReader
 
 
 class NxEmNxsHfiveSubParser:
@@ -109,33 +109,20 @@ class NxEmNxsHfiveSubParser:
             apex = HdfFiveEdaxApexReader(self.file_path)
             apex.parse_and_normalize()
             self.process_into_template(apex.tmp, template)
-        """
         elif hfive_parser_type == "edax":
-            with h5py.File(f"{self.file_path}", "r") as h5r:
-                grp_nms = list(h5r["/"])
-                for grp_nm in grp_nms:
-                    if grp_nm not in ["Version", "Manufacturer"]:
-                        edax_oim = HdfFiveEdaxOimAnalysisReader(self.file_path)
-                        edax_oim.parse_and_normalize_group(
-                            h5r,
-                            f"/{grp_nm}",
-                            self.cache_ebsd)
+            edax = HdfFiveEdaxOimAnalysisReader(self.file_path)
+            edax.parse_and_normalize()
+            self.process_into_template(edax.tmp, template)
         elif hfive_parser_type == "hebsd":
-            with h5py.File(f"{self.file_path}", "r") as h5r:
-                grp_nms = list(h5r["/"])
-                for grp_nm in grp_nms:
-                    if grp_nm not in ["Version", "Manufacturer"]:
-                        edax_oim = HdfFiveCommunityReader(self.file_path)
-                        edax_oim.parse_and_normalize_group(
-                            h5r,
-                            f"/{grp_nm}",
-                            self.cache_ebsd)
+            ebsd = HdfFiveCommunityReader(self.file_path)
+            ebsd.parse_and_normalize()
+            self.process_into_template(ebsd.tmp, template)
         elif hfive_parser_type == "emsoft":
             return template
         else:  # none or something unsupported
             return template
 
-        for key, val in self.cache_ebsd.items():
+        for key, val in self.cache.items():
             print(f"{key}, type: {type(val)}, shape: {np.shape(val)}")
 
         if self.cache["is_filled"] is True:
@@ -143,29 +130,30 @@ class NxEmNxsHfiveSubParser:
             self.process_roi_xmap(template)
             self.process_roi_phases(template)
             self.process_roi_inverse_pole_figures(template)
-        """
         return template
 
     def identify_hfive_type(self):
         """Identify if HDF5 file matches a known format for which a subparser exists."""
+        # tech partner formats used for measurement
         hdf = HdfFiveOxfordReader(f"{self.file_path}")
         if hdf.supported is True:
             return "oxford"
-        # hdf = HdfFiveEdaxOimAnalysisReader(f"{self.file_path}")
-        # if hdf.supported is True:
-        #     return "edax"
+        hdf = HdfFiveEdaxOimAnalysisReader(f"{self.file_path}")
+        if hdf.supported is True:
+            return "edax"
         hdf = HdfFiveEdaxApexReader(f"{self.file_path}")
         if hdf.supported is True:
             return "apex"
         hdf = HdfFiveBrukerEspritReader(f"{self.file_path}")
         if hdf.supported is True:
             return "bruker"
+        hdf = HdfFiveCommunityReader(f"{self.file_path}")
+        if hdf.supported is True:
+            return "hebsd"
+        # computer simulation tools
         # hdf = HdfFiveEmSoftReader(f"{self.file_path}")
         # if hdf.supported is True:
         #     return "emsoft"
-        # hdf = HdfFiveCommunityReader(f"{self.file_path}")
-        # if hdf.supported is True:
-        #     return "hebsd"
         return None
 
     def process_into_template(self, inp: dict, template: dict) -> dict: