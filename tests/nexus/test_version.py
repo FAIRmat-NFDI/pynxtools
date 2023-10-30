defc53a475b29c56d6fde9adf58b5d4ecad69e81
domna <florian.dobener@physik.hu-berlin.de>
2023-10-30 14:59:11 +0100

    Updates tests

--- tests/nexus/test_version.py
+++ tests/nexus/test_version.py
@@ -1,22 +1,14 @@
 """
 Tests the version retrieval for the nexus definitions submodule
 """
-from subprocess import run
-from pynxtools.definitions.dev_tools.globals.nxdl import get_vcs_version
+from pynxtools import get_nexus_version
 
 
-def test_git_is_runnable():
-    """
-    Check if git is generally runnable in this environment
-    """
-    run(["git", "describe", "--tags"], check=True)
-
-
-def test_get_vcs_version():
+def test_get_nexus_version():
     """
     Tests if we get a version string from nexus definitions
     """
-    version = get_vcs_version()
+    version = get_nexus_version()
 
     assert version is not None
     assert version
