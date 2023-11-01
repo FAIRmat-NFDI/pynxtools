ab8f360a41af5179a0351897931a9cc8a545efeb
domna <florian.dobener@physik.hu-berlin.de>
2023-11-01 07:48:37 +0100

    Updates version test

--- tests/nexus/test_version.py
+++ tests/nexus/test_version.py
@@ -13,4 +13,4 @@ def test_get_nexus_version():
     version = get_nexus_version()
 
     assert version is not None
-    assert re.match(r"v\d{4}\.\d{2}\.dev\d+\+g[a-z0-9]", version)
+    assert re.match(r"v\d{4}\.\d{2}\.post1\.dev\d+\+g[a-z0-9]", version)
