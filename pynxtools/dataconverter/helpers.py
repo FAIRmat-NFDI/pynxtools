6ba8e2e945c4be3dfa6288080e561d8ba8049d0b
Sandor Brockhauser <sandor.brockhauser@xfel.eu>
2024-03-26 12:36:04 +0100

    change to lxml

--- pynxtools/dataconverter/helpers.py
+++ pynxtools/dataconverter/helpers.py
@@ -20,11 +20,11 @@
 import json
 import logging
 import re
-import xml.etree.ElementTree as ET
 from datetime import datetime, timezone
 from typing import Any, Callable, List, Optional, Tuple, Union
 
 import h5py
+import lxml.etree as ET
 import numpy as np
 from ase.data import chemical_symbols
 
@@ -63,6 +63,9 @@ def get_all_defined_required_children_for_elem(xml_element):
     """Gets all possible inherited required children for a given NXDL element"""
     list_of_children_to_add = set()
     for child in xml_element:
+        tag = remove_namespace_from_tag(child.tag)
+        if tag not in ('group','field','attribute'):
+            continue
         child.set("nxdlbase_class", xml_element.get("nxdlbase_class"))
         if child.attrib and get_required_string(child) == "required":
             tag = remove_namespace_from_tag(child.tag)
@@ -637,6 +640,8 @@ def validate_data_dict(template, data, nxdl_root: ET.Element):
 def remove_namespace_from_tag(tag):
     """Helper function to remove the namespace from an XML tag."""
 
+    if not isinstance(tag,str):
+        return ""
     return tag.split("}")[-1]
 
 
