36feb2cf79a393a3be7cd814d7f483e8b9b743fa
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2024-06-06 12:20:47 +0200

    reinitialize git submodule

--- src/pynxtools/definitions/dev_tools/docs/__init__.py
+++
@@ -1,4 +0,0 @@
-from .anchor_list import AnchorRegistry  # noqa F401
-from .nxdl import NXClassDocGenerator  # noqa F401
-from .nxdl_index import nxdl_indices  # noqa F401
-from .xsd import XSDDocGenerator  # noqa F401
--- src/pynxtools/definitions/dev_tools/globals/urls.py
+++
@@ -1,2 +0,0 @@
-REPO_URL = "https://github.com/FAIRmat-NFDI/nexus_definitions/tree/fairmat"
-MANUAL_URL = "https://manual.nexusformat.org"
