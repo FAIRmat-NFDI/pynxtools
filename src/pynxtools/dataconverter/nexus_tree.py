6c98778b42d6700eaa992fda7adbd73bee5d97a7
domna <florian.dobener@physik.hu-berlin.de>
2024-06-06 12:57:11 +0200

    Only populate tree if parents are present

--- src/pynxtools/dataconverter/nexus_tree.py
+++ src/pynxtools/dataconverter/nexus_tree.py
@@ -522,7 +522,6 @@ class NexusNode(NodeMixin):
         name = xml_elem.attrib.get("name")
 
         inheritance_chain = [xml_elem]
-<<<<<<< HEAD
         inheritance = iter(self.inheritance)
         for elem in inheritance:
             # Walk until the file the xml_elem is part of
@@ -575,29 +574,6 @@ class NexusNode(NodeMixin):
 
             if inherited_elem and inherited_elem[0] not in inheritance_chain:
                 inheritance_chain.append(inherited_elem[0])
-=======
-        for elem in self.inheritance:
-<<<<<<< HEAD
-            for parent in get_all_parents_for(elem):
-                inherited_elem = parent.xpath(
-                    f"nx:group[@type='{xml_elem.attrib['type']}' and @name='{name}']"
-                    if name is not None
-                    else f"nx:group[@type='{xml_elem.attrib['type']}']",
-                    namespaces=namespaces,
-                )
-                if inherited_elem and inherited_elem[0] not in inheritance_chain:
-                    inheritance_chain.append(inherited_elem[0])
->>>>>>> 92a2be08 (Insert extends parents into the inheritance chain)
-=======
-            inherited_elem = elem.xpath(
-                f"nx:group[@type='{xml_elem.attrib['type']}' and @name='{name}']"
-                if name is not None
-                else f"nx:group[@type='{xml_elem.attrib['type']}']",
-                namespaces=namespaces,
-            )
-            if inherited_elem and inherited_elem[0] not in inheritance_chain:
-                inheritance_chain.append(inherited_elem[0])
->>>>>>> 0e3b8441 (Automatically populate tree from appdef parents)
         bc_xml_root, _ = get_nxdl_root_and_path(xml_elem.attrib["type"])
         inheritance_chain.append(bc_xml_root)
         inheritance_chain += get_all_parents_for(bc_xml_root)
@@ -632,11 +608,8 @@ class NexusNode(NodeMixin):
                 name_type=name_type,
                 type=tag,
                 optionality=default_optionality,
-<<<<<<< HEAD
                 nxdl_base=xml_elem.base,
                 inheritance=[xml_elem],
-=======
->>>>>>> 92a2be08 (Insert extends parents into the inheritance chain)
             )
         elif tag == "group":
             name = xml_elem.attrib.get("name")
@@ -653,23 +626,15 @@ class NexusNode(NodeMixin):
                 nx_class=xml_elem.attrib["type"],
                 inheritance=inheritance_chain,
                 optionality=default_optionality,
-<<<<<<< HEAD
                 nxdl_base=xml_elem.base,
-=======
->>>>>>> 92a2be08 (Insert extends parents into the inheritance chain)
             )
         elif tag == "choice":
             current_elem = NexusChoice(
                 parent=self,
                 name=xml_elem.attrib["name"],
-<<<<<<< HEAD
                 name_type=name_type,
                 optionality=default_optionality,
                 nxdl_base=xml_elem.base,
-=======
-                variadic=contains_uppercase(xml_elem.attrib["name"]),
-                optionality=default_optionality,
->>>>>>> 92a2be08 (Insert extends parents into the inheritance chain)
             )
         else:
             # TODO: Tags: link
@@ -705,12 +670,7 @@ class NexusNode(NodeMixin):
                     namespaces=namespaces,
                 )
             if xml_elem:
-<<<<<<< HEAD
                 return self.add_node_from(xml_elem[0])
-=======
-                new_node = self.add_node_from(xml_elem[0])
-                return new_node
->>>>>>> 92a2be08 (Insert extends parents into the inheritance chain)
         return None
 
 
@@ -1178,21 +1138,12 @@ def populate_tree_from_parents(node: NexusNode):
         node (NexusNode):
             The current node from which to populate the tree.
     """
-<<<<<<< HEAD
     for child in node.get_all_direct_children_names(only_appdef=True):
         child_node = node.search_add_child_for(child)
         populate_tree_from_parents(child_node)
 
 
 def generate_tree_from(appdef: str, set_root_attr: bool = True) -> NexusNode:
-=======
-    for child in node.get_all_children_names(only_appdef=True):
-        child_node = node.search_child_with_name(child)
-        populate_tree_from_parents(child_node)
-
-
-def generate_tree_from(appdef: str) -> NexusNode:
->>>>>>> 0e3b8441 (Automatically populate tree from appdef parents)
     """
     Generates a NexusNode tree from an application definition.
     NexusNode is based on anytree nodes and anytree's functions can be used
@@ -1245,11 +1196,9 @@ def generate_tree_from(appdef: str) -> NexusNode:
         variadic=False,
         parent=None,
         inheritance=appdef_inheritance_chain,
-<<<<<<< HEAD
         nxdl_base=appdef_xml_root.base,
-=======
->>>>>>> 284333e5 (Read extends keyword from file)
     )
+
     # Set root attributes
     if set_root_attr:
         nx_root, _ = get_nxdl_root_and_path("NXroot")
@@ -1261,11 +1210,7 @@ def generate_tree_from(appdef: str) -> NexusNode:
     add_children_to(tree, entry)
 
     # Add all fields and attributes from the parent appdefs
-<<<<<<< HEAD
     if len(appdef_inheritance_chain) > 1:
         populate_tree_from_parents(tree)
-=======
-    populate_tree_from_parents(tree)
 
->>>>>>> 0e3b8441 (Automatically populate tree from appdef parents)
     return tree
