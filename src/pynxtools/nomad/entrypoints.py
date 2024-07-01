94f0766a1cef070e251a02ae18409bfe18c1b569
domna <florian.dobener@physik.hu-berlin.de>
2024-07-01 17:48:59 +0200

    Copy parser and schema from nomad

---
+++ src/pynxtools/nomad/entrypoints.py
@@ -0,0 +1,27 @@
+from nomad.config.models.plugins import ParserEntryPoint, SchemaPackageEntryPoint
+
+
+class NexusParserEntryPoint(ParserEntryPoint):
+    def load(self):
+        from pynxtools.nomad.parser import NexusParser
+
+        return NexusParser(**self.dict())
+
+
+class NexusSchemaEntryPoint(SchemaPackageEntryPoint):
+    def load(self):
+        from pynxtools.nomad.schema import nexus_metainfo_package
+
+        return nexus_metainfo_package
+
+
+nexus_schema = NexusSchemaEntryPoint(
+    name="NeXus",
+    description="The NeXus metainfo package.",
+)
+
+nexus_parser = NexusParserEntryPoint(
+    name="pynxtools parser",
+    description="A parser for nexus files.",
+    mainfile_name_re=r".*\.nxs",
+)
