cbc1850bfc7d15469ca3d88756d25995acf163bd
domna <florian.dobener@physik.hu-berlin.de>
2024-07-05 14:32:32 +0200

    Move get_vcs to build wrapper

--- src/pynxtools/_build_wrapper.py
+++ src/pynxtools/_build_wrapper.py
@@ -4,14 +4,42 @@ containing the nexus definitions verison.
 """
 
 import os
-
-from pynxtools import get_vcs_version
+from subprocess import CalledProcessError, run
+from typing import Optional
 
 try:
     from setuptools import build_meta as _orig
     from setuptools.build_meta import *  # pylint: disable=wildcard-import,unused-wildcard-import
 except ImportError:
     pass
+
+
+def get_vcs_version(tag_match="*[0-9]*") -> Optional[str]:
+    """
+    The version of the Nexus standard and the NeXus Definition language
+    based on git tags and commits
+    """
+    try:
+        return (
+            run(
+                [
+                    "git",
+                    "describe",
+                    "--dirty",
+                    "--tags",
+                    "--long",
+                    "--match",
+                    tag_match,
+                ],
+                cwd=os.path.join(os.path.dirname(__file__), "../pynxtools/definitions"),
+                check=True,
+                capture_output=True,
+            )
+            .stdout.decode("utf-8")
+            .strip()
+        )
+    except (FileNotFoundError, CalledProcessError):
+        return None
 
 
 def _write_version_to_metadata():
