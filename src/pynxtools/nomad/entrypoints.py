dda77c9f2db44f0f18d71b7b06c65cdfa788a0a2
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2024-10-14 14:53:23 +0200

    remove entrypoints for examples that have been moved to plugins

--- src/pynxtools/nomad/entrypoints.py
+++ src/pynxtools/nomad/entrypoints.py
@@ -65,50 +65,6 @@ nexus_parser = NexusParserEntryPoint(
     mainfile_mime_re="application/x-hdf5",
 )
 
-apm_example = ExampleUploadEntryPoint(
-    title="Atom Probe Microscopy",
-    category="FAIRmat examples",
-    description="""
-        This example presents the capabilities of the NOMAD platform to store and standardize atom probe data.
-        It shows the generation of a NeXus file according to the
-        [NXapm](https://fairmat-nfdi.github.io/nexus_definitions/classes/contributed_definitions/NXapm.html#nxapm)
-        application definition and a successive analysis of an example data set.
-        The example contains a small atom probe dataset from an experiment with a LEAP instrument to get you started
-        and keep the size of your NOMAD installation small. Once started, we recommend changing the respective
-        input file in the NOMAD Oasis ELN to run the example with your own datasets.
-    """,
-    url="https://download-directory.github.io/?url=https://github.com/FAIRmat-NFDI/pynxtools-apm/tree/main/examples/nomad",
-    local_path="examples/data/uploads/apm.zip",
-)
-
-ellips_example = ExampleUploadEntryPoint(
-    title="Ellipsometry",
-    category="FAIRmat examples",
-    description="""
-        This example presents the capabilities of the NOMAD platform to store and standardize ellipsometry data.
-      It shows the generation of a NeXus file according to the [NXellipsometry](https://manual.nexusformat.org/classes/contributed_definitions/NXellipsometry.html#nxellipsometry)
-      application definition and a successive analysis of a SiO2 on Si Psi/Delta measurement.
-    """,
-    url="https://download-directory.github.io/?url=https://github.com/FAIRmat-NFDI/pynxtools-ellips/tree/main/examples/nomad",
-    local_path="examples/data/uploads/ellips.zip",
-)
-
-em_example = ExampleUploadEntryPoint(
-    title="Electron Microscopy",
-    category="FAIRmat examples",
-    description="""
-        This example presents the capabilities of the NOMAD platform to store and standardize electron microscopy.
-        It shows the generation of a NeXus file according to the
-        [NXem](https://fairmat-nfdi.github.io/nexus_definitions/classes/contributed_definitions/NXem.html#nxem)
-        application definition.
-        The example contains a small set of electron microscopy datasets to get started and keep the size of your
-        NOMAD installation small. Ones started, we recommend to change the respective input file in the NOMAD Oasis
-        ELN to run the example with your own datasets.
-    """,
-    url="https://download-directory.github.io/?url=https://github.com/FAIRmat-NFDI/pynxtools-em/tree/main/examples/nomad",
-    local_path="examples/data/uploads/em.zip",
-)
-
 iv_temp_example = ExampleUploadEntryPoint(
     title="Sensor Scan - IV Temperature Curve",
     category="FAIRmat examples",
@@ -142,17 +98,4 @@ sts_example = ExampleUploadEntryPoint(
     """,
     url="https://download-directory.github.io/?url=https://github.com/FAIRmat-NFDI/pynxtools-stm/tree/main/examples/nomad/sts",
     local_path="examples/data/uploads/sts.zip",
-)
-
-xps_example = ExampleUploadEntryPoint(
-    title="X-ray Photoelectron Spectroscopy (XPS)",
-    category="FAIRmat examples",
-    description="""
-        This example presents the capabilities of the NOMAD platform to store and standardize X-ray Photoelectron Spectroscopy XPS data.
-        It shows the generation of a NeXus file according to the
-        [NXmpes](https://manual.nexusformat.org/classes/contributed_definitions/NXmpes.html#nxmpes)
-        application definition and a successive analysis of an example data set.
-    """,
-    url="https://download-directory.github.io/?url=https://github.com/FAIRmat-NFDI/pynxtools-xps/tree/main/examples/nomad",
-    local_path="examples/data/uploads/xps.zip",
 )
