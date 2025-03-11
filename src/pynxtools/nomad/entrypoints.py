d4852426973b772e129410a4e315443d34a26a56
GinzburgLev <ginzburglev95@gmail.com>
2025-03-11 15:52:54 +0100

    No separate NXsensor_scan app anymore, instead NeXus app was updated.

--- src/pynxtools/nomad/entrypoints.py
+++ src/pynxtools/nomad/entrypoints.py
@@ -71,37 +71,29 @@ from nomad.config.models.ui import (
     Column,
     Menu,
     MenuItemHistogram,
-    MenuItemOption,
     MenuItemPeriodicTable,
     MenuItemTerms,
+    MenuSizeEnum,
     SearchQuantities,
 )
 
 schema = "pynxtools.nomad.schema.Root"
 
-nxdefs_inheriting_from_sensorscan = [
-    "NXsensor_scan",
-    "NXiv_temp",
-    "NXspm",
-    "NXsts",
-    "NXafm",
-    "NXstm",
-]
 
-nxsensor_scan_app = AppEntryPoint(
-    name="NXsensor_scan App",
-    description="App for NXsensor_scan and inheriting from it classes.",
+nexus_app = AppEntryPoint(
+    name="NeXus App",
+    description="Simple Generic NeXus app.",
     app=App(
         # Label of the App
-        label="Simple Scan",
+        label="NeXus",
         # Path used in the URL, must be unique
-        path="nxsensor_scan",
+        path="nexusapp",
         # Used to categorize apps in the explore menu
         category="Experiment",
         # Brief description used in the app menu
-        description="Search simple scan experiments (NXsensor_scan and inheriting classes), including output of NOMAD CAMELS",
+        description="A simple search app customized for generic NeXus data.",
         # Longer description that can also use markdown
-        readme="This page allows to search for generic experimental entries corresponding to NXsensor_scan and inheriting from it classes, including output of NOMAD CAMELS. It is similar to the entries search, but with reduced filter set, modified menu on the left and different shown columns. The dashboard directly shows useful interactive statistics about the data",
+        readme="This page allows to search for generic NeXus Experiment Entries. It is similar to the entries search, but with reduced filter set, modified menu on the left and different shown columns. The dashboard directly shows useful interactive statistics about the data",
         # If you want to use quantities from a custom schema, you need to load
         # the search quantities from it first here. Note that you can use a glob
         # syntax to load the entire package, or just a single schema from a
@@ -147,25 +139,19 @@ nxsensor_scan_app = AppEntryPoint(
                 search_quantity=f"data.ENTRY[*].definition__field#{schema}",
                 selected=True,
             ),
-            Column(
-                title="Protocol",
-                search_quantity=f"data.ENTRY[*].NOTE[?name=='protocol'].file_name__field#{schema}#str",
-                selected=False,
-            ),
         ],
         # Dictionary of search filters that are always enabled for queries made
         # within this app. This is especially important to narrow down the
         # results to the wanted subset.
-        filters_locked={
-            f"data.ENTRY.definition__field#{schema}": nxdefs_inheriting_from_sensorscan,
-        },
-        # filters_locked={"section_defs.definition_qualified_name": [schema]},
+        filters_locked={"section_defs.definition_qualified_name": [schema]},
         # Controls the menu shown on the left
         menu=Menu(
+            size=MenuSizeEnum.MD,
             title="Menu",
             items=[
                 Menu(
                     title="Elements",
+                    size=MenuSizeEnum.XXL,
                     items=[
                         MenuItemPeriodicTable(
                             search_quantity="results.material.elements",
@@ -180,13 +166,42 @@ nxsensor_scan_app = AppEntryPoint(
                             width=6,
                             options=0,
                         ),
+                        MenuItemTerms(
+                            search_quantity="results.material.chemical_formula_reduced",
+                            width=6,
+                            options=0,
+                        ),
+                        MenuItemTerms(
+                            search_quantity="results.material.chemical_formula_anonymous",
+                            width=6,
+                            options=0,
+                        ),
                         MenuItemHistogram(
                             x="results.material.n_elements",
                         ),
                     ],
                 ),
                 Menu(
+                    title="Experiment type",
+                    size=MenuSizeEnum.LG,
+                    items=[
+                        MenuItemTerms(
+                            title="Entry Type",
+                            search_quantity=f"entry_type",
+                            width=12,
+                            options=12,
+                        ),
+                        MenuItemTerms(
+                            title="NeXus Class",
+                            search_quantity=f"data.ENTRY.definition__field#{schema}",
+                            width=12,
+                            options=12,
+                        ),
+                    ],
+                ),
+                Menu(
                     title="Instruments",
+                    size=MenuSizeEnum.LG,
                     items=[
                         MenuItemTerms(
                             title="Model",
@@ -204,6 +219,7 @@ nxsensor_scan_app = AppEntryPoint(
                 ),
                 Menu(
                     title="Samples",
+                    size=MenuSizeEnum.LG,
                     items=[
                         MenuItemTerms(
                             title="Name",
@@ -220,222 +236,96 @@ nxsensor_scan_app = AppEntryPoint(
                     ],
                 ),
                 Menu(
-                    title="Authors",
+                    title="Authors / Origin",
+                    size=MenuSizeEnum.LG,
                     items=[
                         MenuItemTerms(
-                            title="Name",
+                            title="Entry Author",
                             search_quantity=f"data.ENTRY.USER.name__field#{schema}",
                             width=12,
-                            options=12,
+                            options=5,
+                        ),
+                        MenuItemTerms(
+                            title="User ID / Entry Author",
+                            search_quantity=f"data.ENTRY.userID.name__field#{schema}#str",
+                            width=12,
+                            options=5,
+                        ),
+                        MenuItemTerms(
+                            title="Upload Author",
+                            search_quantity=f"authors.name",
+                            width=12,
+                            options=5,
                         ),
                         MenuItemTerms(
                             title="Affiliation",
                             search_quantity=f"data.ENTRY.USER.affiliation__field#{schema}",
                             width=12,
-                            options=12,
+                            options=5,
                         ),
                     ],
                 ),
-                Menu(
-                    title="Protocols (NOMAD CAMELS)",
-                    items=[
-                        MenuItemTerms(
-                            title="CAMELS files",
-                            search_quantity=f"data.ENTRY.PROCESS.program__field#{schema}",
-                            width=12,
-                            options={
-                                "NOMAD CAMELS": MenuItemOption(
-                                    label="NOMAD CAMELS entries only",
-                                ),
-                            },
-                            show_header=False,
-                            show_input=False,
-                        ),
-                        MenuItemTerms(
-                            title="Protocols (only for CAMELS files)",
-                            search_quantity=f"data.ENTRY.NOTE.file_name__field#{schema}#str",
-                            width=12,
-                            options=12,
-                        ),
-                    ],
+                MenuItemHistogram(
+                    title="Start Time",
+                    x=f"data.ENTRY.start_time#{schema}",
+                    autorange=True,
+                ),
+                MenuItemHistogram(
+                    title="Upload Creation Time",
+                    x=f"upload_create_time",
+                    autorange=True,
                 ),
             ],
         ),
         # Controls the default dashboard shown in the search interface
         dashboard={
             "widgets": [
-                {
-                    "type": "histogram",
-                    "show_input": False,
-                    "autorange": True,
-                    "nbins": 30,
-                    "scale": "linear",
-                    "quantity": f"data.ENTRY.start_time#{schema}",
-                    "title": "Start Time",
-                    "layout": {
-                        "lg": {"minH": 3, "minW": 3, "h": 6, "w": 10, "y": 0, "x": 0}
-                    },
-                },
-                {
-                    "type": "terms",
-                    "show_input": False,
-                    "scale": "linear",
-                    "quantity": f"entry_type",
-                    "title": "Entry Type",
-                    "layout": {
-                        "lg": {"minH": 3, "minW": 3, "h": 6, "w": 5, "y": 6, "x": 0}
-                    },
-                },
-                {
-                    "type": "terms",
-                    "show_input": False,
-                    "scale": "linear",
-                    "quantity": f"data.ENTRY.SAMPLE.sample_id__field#{schema}",
-                    "title": "Sample ID",
-                    "layout": {
-                        "lg": {"minH": 3, "minW": 3, "h": 12, "w": 4, "y": 0, "x": 14}
-                    },
-                },
-                {
-                    "type": "terms",
-                    "show_input": False,
-                    "scale": "linear",
-                    "quantity": f"data.ENTRY.USER.name__field#{schema}",
-                    "title": "Author",
-                    "layout": {
-                        "lg": {"minH": 3, "minW": 3, "h": 6, "w": 5, "y": 6, "x": 5}
-                    },
-                },
-                {
-                    "type": "terms",
-                    "show_input": False,
-                    "scale": "linear",
-                    "quantity": f"data.ENTRY.SAMPLE.name__field#{schema}",
-                    "title": "Sample",
-                    "layout": {
-                        "lg": {"minH": 3, "minW": 3, "h": 12, "w": 4, "y": 0, "x": 10}
-                    },
-                },
-            ]
-        },
-    ),
-)
-
-nexus_app = AppEntryPoint(
-    name="NexusApp",
-    description="Simple Generic NeXus app.",
-    app=App(
-        # Label of the App
-        label="NeXus",
-        # Path used in the URL, must be unique
-        path="nexusapp",
-        # Used to categorize apps in the explore menu
-        category="Experiment",
-        # Brief description used in the app menu
-        description="A simple search app customized for generic NeXus data.",
-        # Longer description that can also use markdown
-        readme="This is a simple App to support basic search for NeXus based Experiment Entries.",
-        # If you want to use quantities from a custom schema, you need to load
-        # the search quantities from it first here. Note that you can use a glob
-        # syntax to load the entire package, or just a single schema from a
-        # package.
-        search_quantities=SearchQuantities(
-            include=[f"*#{schema}"],
-        ),
-        # Controls which columns are shown in the results table
-        columns=[
-            Column(quantity="entry_id", selected=True),
-            Column(quantity=f"entry_type", selected=True),
-            Column(
-                title="definition",
-                quantity=f"data.ENTRY[*].definition__field#{schema}",
-                selected=True,
-            ),
-            Column(
-                title="start_time",
-                quantity=f"data.ENTRY[*].start_time__field#{schema}",
-                selected=True,
-            ),
-            Column(
-                title="title",
-                quantity=f"data.ENTRY[*].title__field#{schema}",
-                selected=True,
-            ),
-        ],
-        # Dictionary of search filters that are always enabled for queries made
-        # within this app. This is especially important to narrow down the
-        # results to the wanted subset. Any available search filter can be
-        # targeted here. This example makes sure that only entries that use
-        # MySchema are included.
-        filters_locked={"section_defs.definition_qualified_name": [schema]},
-        # Controls the menu shown on the left
-        menu=Menu(
-            title="Material",
-            items=[
-                Menu(
-                    title="elements",
-                    items=[
-                        MenuItemPeriodicTable(
-                            quantity="results.material.elements",
-                        ),
-                        MenuItemTerms(
-                            quantity="results.material.chemical_formula_hill",
-                            width=6,
-                            options=0,
-                        ),
-                        MenuItemTerms(
-                            quantity="results.material.chemical_formula_iupac",
-                            width=6,
-                            options=0,
-                        ),
-                        MenuItemHistogram(
-                            x="results.material.n_elements",
-                        ),
-                    ],
-                )
-            ],
-        ),
-        # Controls the default dashboard shown in the search interface
-        dashboard={
-            "widgets": [
-                {
-                    "type": "histogram",
-                    "show_input": False,
-                    "autorange": True,
-                    "nbins": 30,
-                    "scale": "linear",
-                    "quantity": f"data.ENTRY.start_time__field#{schema}",
-                    "title": "Start Time",
-                    "layout": {
-                        "lg": {"minH": 3, "minW": 3, "h": 4, "w": 12, "y": 0, "x": 0}
-                    },
-                },
-                {
-                    "type": "terms",
-                    "show_input": False,
-                    "scale": "linear",
-                    "quantity": f"entry_type",
-                    "title": "Entry Type",
-                    "layout": {
-                        "lg": {"minH": 3, "minW": 3, "h": 8, "w": 4, "y": 0, "x": 12}
-                    },
-                },
-                {
-                    "type": "terms",
-                    "show_input": False,
-                    "scale": "linear",
-                    "quantity": f"data.ENTRY.definition__field#{schema}",
-                    "title": "Definition",
-                    "layout": {
-                        "lg": {"minH": 3, "minW": 3, "h": 8, "w": 4, "y": 0, "x": 16}
-                    },
-                },
                 {
                     "type": "periodic_table",
                     "scale": "linear",
                     "quantity": f"results.material.elements",
                     "layout": {
-                        "lg": {"minH": 3, "minW": 3, "h": 4, "w": 12, "y": 4, "x": 0}
+                        "lg": {"minH": 3, "minW": 3, "h": 7, "w": 10, "y": 0, "x": 0}
+                    },
+                },
+                {
+                    "type": "terms",
+                    "show_input": True,
+                    "scale": "linear",
+                    "quantity": f"entry_type",
+                    "title": "Entry Type",
+                    "layout": {
+                        "lg": {"minH": 3, "minW": 3, "h": 7, "w": 4, "y": 0, "x": 10}
+                    },
+                },
+                {
+                    "type": "terms",
+                    "show_input": True,
+                    "scale": "linear",
+                    "quantity": f"data.ENTRY.definition__field#{schema}",
+                    "title": "NeXus Class",
+                    "layout": {
+                        "lg": {"minH": 3, "minW": 3, "h": 7, "w": 4, "y": 0, "x": 14}
+                    },
+                },
+                {
+                    "type": "terms",
+                    "show_input": True,
+                    "scale": "linear",
+                    "quantity": f"data.ENTRY.USER.name__field#{schema}",
+                    "title": "Author",
+                    "layout": {
+                        "lg": {"minH": 3, "minW": 3, "h": 7, "w": 4, "y": 0, "x": 18}
+                    },
+                },
+                {
+                    "type": "terms",
+                    "show_input": True,
+                    "scale": "linear",
+                    "quantity": f"data.ENTRY.SAMPLE.name__field#{schema}",
+                    "title": "Sample",
+                    "layout": {
+                        "lg": {"minH": 3, "minW": 3, "h": 7, "w": 4, "y": 0, "x": 22}
                     },
                 },
             ]
