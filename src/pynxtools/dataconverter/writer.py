d9218ff2cd35dbe2f7da799cc833f4210c220ab0
Rubel <rubel.mozumder@outlook.com>
2025-04-10 16:42:07 +0200

    Add proper warning massage for incorrect group symantic.

--- src/pynxtools/dataconverter/writer.py
+++ src/pynxtools/dataconverter/writer.py
@@ -247,9 +247,16 @@ class Writer:
             grp = parent.create_group(parent_path_hdf5)
 
             attrs = self.__nxdl_to_attrs(parent_path)
-
             if attrs is not None:
-                grp.attrs["NX_class"] = attrs["type"]
+                try:
+                    grp.attrs["NX_class"] = attrs["type"]
+                except KeyError as e:
+                    raise NxdlAttributeNotFoundError(
+                        f"NXDL attribute 'type' not found for group : {parent_path} "
+                        f"Hint: Follow convention fixednameVARIACKPART[fixedname_given_name] "
+                        f"with exact group name in NXDL file."
+                    ) from e
+
             return grp
         return self.output_nexus[parent_path_hdf5]
 
@@ -258,14 +265,11 @@ class Writer:
 
         hdf5_links_for_later = []
 
-        def add_units_key(dataset, path):
-            units_key = f"{path}/@units"
-            if units_key in self.data.keys() and self.data[units_key] is not None:
-                dataset.attrs["units"] = self.data[units_key]
-
+        unit_and_attr = {}
         for path, value in self.data.items():
             try:
-                if path[path.rindex("/") + 1 :] == "@units":
+                if path[path.rindex("/") + 1 :].startswith("@"):
+                    unit_and_attr[path] = value
                     continue
 
                 entry_name = helpers.get_name_from_data_dict_entry(
@@ -275,10 +279,11 @@ class Writer:
                     data = value
                 else:
                     continue
-
-                if entry_name[0] != "@":
+                # Handle field and group
+                if entry_name and entry_name[0] != "@":
                     grp = self.ensure_and_get_parent_node(
-                        path, self.data.undocumented.keys()
+                        path,
+                        self.data.undocumented.keys(),
                     )
                     if isinstance(data, dict):
                         if "compress" in data.keys():
@@ -291,49 +296,38 @@ class Writer:
                             )
                     else:
                         dataset = grp.create_dataset(entry_name, data=data)
+
             except InvalidDictProvided as exc:
                 print(str(exc))
             except Exception as exc:
                 raise IOError(
                     f"Unknown error occured writing the path: {path} "
-                    f"with the following message: {str(exc)}"
+                    f"with the following message: {str(exc)}\n"
                 ) from exc
+
+        # Handle unit and attribute
+        for path, value in unit_and_attr.items():
+            hdf5_path = helpers.convert_data_dict_path_to_hdf5_path(path)
+            parent, entry_name = hdf5_path.rsplit("/", 1)
+            try:
+                dataset_or_grp = self.output_nexus[parent + "/"]
+            except KeyError as exc:
+                logger.warning(
+                    "No path '%s' available to attached Attribute.", hdf5_path
+                )
+                dataset_or_grp = None
+            if dataset_or_grp is None:
+                continue
+            if path.endswith("/@units"):
+                dataset_or_grp.attrs["units"] = value
+            else:
+                dataset_or_grp.attrs[entry_name[1:]] = value
 
         for links in hdf5_links_for_later:
             dataset = handle_dicts_entries(*links)
             if dataset is None:
                 # If target of a link is invalid to be linked
                 del self.data[links[-1]]
-
-        for path, value in self.data.items():
-            try:
-                if path[path.rindex("/") + 1 :] == "@units":
-                    continue
-
-                entry_name = helpers.get_name_from_data_dict_entry(
-                    path[path.rindex("/") + 1 :]
-                )
-                if is_not_data_empty(value):
-                    data = value
-                else:
-                    continue
-
-                if entry_name[0] != "@":
-                    path_hdf5 = helpers.convert_data_dict_path_to_hdf5_path(path)
-
-                    add_units_key(self.output_nexus[path_hdf5], path)
-                else:
-                    # consider changing the name here the lvalue can also be group!
-                    dataset = self.ensure_and_get_parent_node(
-                        path, self.data.undocumented.keys()
-                    )
-                    dataset.attrs[entry_name[1:]] = data
-            except Exception as exc:
-                raise IOError(
-                    f"Unknown error occured writing the path: {path}"
-                    f", while writing the value: {value} "
-                    f"with the following message: {str(exc)}"
-                ) from exc
 
     def write(self):
         """Writes the NeXus file with previously validated data from the reader with NXDL attrs."""
