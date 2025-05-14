609ede67b188d3bafb49f5c84be86f1825848239
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2025-05-14 17:38:43 +0200

    add a separate error for names with wrong type and remove such keys

--- tests/dataconverter/test_validation.py
+++ tests/dataconverter/test_validation.py
@@ -773,6 +773,32 @@ TEMPLATE["required"][
         ),
         pytest.param(
             alter_dict(
+                TEMPLATE,
+                "/ENTRY[my_entry]/identified_calibration/identifier_1/some_field",
+                "123",
+            ),
+            [
+                "The type ('group') of the given concept 'identifier_1' conflicts with another "
+                "existing concept of the same name, which is of type 'field').",
+                "The attribute /ENTRY[my_entry]/identified_calibration/identifier_1/some_field will not be written.",
+            ],
+            id="group-instead-of-named-field",
+        ),
+        pytest.param(
+            alter_dict(
+                TEMPLATE,
+                "/ENTRY[my_entry]/identified_calibration",
+                "123",
+            ),
+            [
+                "The type ('field') of the given concept 'identified_calibration' conflicts with another "
+                "existing concept of the same name, which is of type 'group').",
+                "The attribute /ENTRY[my_entry]/identified_calibration will not be written.",
+            ],
+            id="field-instead-of-named-group",
+        ),
+        pytest.param(
+            alter_dict(
                 remove_from_dict(
                     TEMPLATE,
                     "/ENTRY[my_entry]/optional_parent/optional_child",
@@ -1208,7 +1234,10 @@ def test_validate_data_dict(caplog, data_dict, error_messages, request):
     """Unit test for the data validation routine."""
 
     def format_error_message(msg: str) -> str:
-        return msg[msg.rfind("G: ") + 3 :].rstrip("\n")
+        for prefix in ("ERROR:", "WARNING:"):
+            if msg.startswith(prefix):
+                return msg[len(prefix) :].lstrip()
+        return msg
 
     if not error_messages:
         with caplog.at_level(logging.WARNING):
@@ -1220,8 +1249,6 @@ def test_validate_data_dict(caplog, data_dict, error_messages, request):
             "baseclass-field-with-illegal-unit",
             "open-enum-with-new-item",
             "baseclass-open-enum-with-new-item",
-            "variadic-nxcollection",
-            "nonvariadic-nxcollection",
         ):
             with caplog.at_level(logging.INFO):
                 assert validate_dict_against("NXtest", data_dict)[0]
