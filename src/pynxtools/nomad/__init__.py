0c1b9badb99e1feec9bd72c647713c70a8a6be1c
Tanmay2028 <tanmay.kulkarni@fau.de>
2025-07-29 15:55:15 +0200

    Use reasoner and load superclasses from saved inferred ontology

--- src/pynxtools/nomad/__init__.py
+++ src/pynxtools/nomad/__init__.py
@@ -7,21 +7,21 @@ import sys
 server_process = None
 
 def start_ontology_service():
-    """Start the superclasses FastAPI server."""
+    """Start the ontology FastAPI server."""
     uvicorn.run("pynxtools.nomad.ontology_service:app", host="0.0.0.0", port=8089, reload=False)
 
 def stop_ontology_service():
-    """Stop the superclasses FastAPI server."""
+    """Stop the ontology FastAPI server."""
     global server_process
     if server_process is not None and server_process.is_alive():
-        print("Superclasses server shutdown requested.")
+        print("ontology service shutdown requested.")
         server_process.terminate()  # Terminate the process
         server_process.join()       # Wait for the process to finish
     server_process = None
         
 
 def run_ontology_service():
-    """Run the superclasses FastAPI server in a separate process."""
+    """Run the ontology FastAPI server in a separate process."""
     global server_process
     if server_process is None or not server_process.is_alive():
         server_process = multiprocessing.Process(target=start_ontology_service)
