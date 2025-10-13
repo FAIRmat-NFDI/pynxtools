e72c25782acfd702268e08e80d09450c68ad6eb6
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2025-10-13 15:59:33 +0200

    fix linting

--- src/pynxtools/nomad/schema.py
+++ src/pynxtools/nomad/schema.py
@@ -410,7 +410,7 @@ def nxdata_ensure_definition(
     self,
     def_or_name: Property | str,
     *,
-    hint: Optional[str] = None,
+    hint: str | None = None,
 ) -> Property:
     current_cls = section_definitions[f"{_rename_nx_for_nomad('NXdata')}"].section_cls
     if isinstance(def_or_name, str):
