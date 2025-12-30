48d45e5c681a7f8659e0ea5f959ff60a12c2af0b
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2025-12-30 20:04:09 +0100

    Design chunking configurations for different systems

---
+++ src/pynxtools/dataconverter/hdfchunking.py
@@ -0,0 +1,25 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Use-case-specific configurations to optimize performance for chunked storage."""
+
+from typing import Any
+
+CHUNK_CONFIG_DEFAULT: dict[str, Any] = {}
+CHUNK_CONFIG_NOMAD: dict[str, Any] = {}
+CHUNK_CONFIG_GPFS: dict[str, Any] = {}
+CHUNK_CONFIG_LUSTRE: dict[str, Any] = {}
