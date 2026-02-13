5d82133682c9fa9de339ef5e44a8f83c8e0fe8c9
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2026-02-13 23:46:27 +0100

    docstring

--- src/pynxtools/dataconverter/chunk.py
+++ src/pynxtools/dataconverter/chunk.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-"""Configuration defaults of the dataconverter."""
+"""Configuration and utilities for customized chunking and compression."""
 
 import numpy as np
 
