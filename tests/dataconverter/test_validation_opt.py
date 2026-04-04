ae3e7550560cb3e4ea52f013cb7c4cbaf0fb3832
Markus Kühbach <markus.kuehbach@physik.hu-berlin.de>
2026-04-04 02:06:04 +0200

    testing

--- tests/dataconverter/test_validation_opt.py
+++ tests/dataconverter/test_validation_opt.py
@@ -25,14 +25,40 @@ import numpy as np
 import pytest
 # import h5py.h5p as h5p
 from pynxtools.dataconverter.validate_file import validate
+from pathlib import Path
 
 
-# parameterized ["chunked_uncompressed", "chunked_compressed", "contiguous"]:
-def test_validate_file_different_storage_layouts(storage_layout: str, caplog):
+@pytest.mark.parametrize(
+    "storage_layout, error_messages",
+    [
+        pytest.param(
+            "chunked_uncompressed",
+            [
+                ""
+            ],
+            id="chunked-uncompressed",
+        ),
+        pytest.param(
+            "chunked_compressed",
+            [
+                ""
+            ],
+            id="chunked-compressed",
+        ),
+        pytest.param(
+            "contiguous",
+            [
+                ""
+            ],
+            id="contiguous",
+        ),
+    ],
+)
+def test_validate_file_storage_layouts(storage_layout, tmp_path, caplog, error_messages):
     """Test validation of a NeXus/HDF5 with the same content but different storage layout."""
-    file_path = f"{storage_layout}.nxs"  # prefix
-    prng = np.random.default_rng(seed=42)  # deterministic seeding
-    n_values = 1000 * 50 * 50
+    file_path = tmp_path / f"{storage_layout}.nxs"
+    # prng = np.random.default_rng(seed=42)  # deterministic seeding
+    n_values = 100 * 50**2
     with h5py.File(file_path, "w", track_order=True) as h5w:
         gcpl = h5w.id.get_create_plist()
         flags = gcpl.get_link_creation_order()
@@ -45,10 +71,10 @@ def test_validate_file_different_storage_layouts(storage_layout: str, caplog):
             grp.attrs["NX_class"] = f"NX{class_name.lower()}"
         grp = h5w.create_group(f"{trg}")
 
-        chunking = (100, 50, 50)
+        chunking = (10, 50, 50)
         axes = ["indices_image", "axis_j", "axis_i"]
-        real = np.asarray(prng.random(size=n_values), np.float32).reshape(-1, 50, 50)
-        # [0, 1), last is fastest
+        # real = np.asarray(prng.random(size=n_values), np.float32).reshape(-1, 50, 50)
+        real = np.ones((n_values // 50**2, 50, 50), np.float32)
         grp.attrs["NX_class"] = "NXdata"
         grp.attrs["axes"] = axes
         grp.attrs["signal"] = "real"
@@ -74,30 +100,92 @@ def test_validate_file_different_storage_layouts(storage_layout: str, caplog):
         observed_infos = [
             rec.getMessage() for rec in caplog.records if rec.levelno == logging.INFO
         ]
+        for rec in observed_infos:
+            print(rec)
 
     os.remove(file_path)
 
 
-def test_validate_file_positive_int(storage_layout: str, positive: bool, dtype, caplog):
-    file_path = f"{storage_layout}.nxs"  # prefix
+@pytest.mark.parametrize(
+    "storage_layout, positive, dtype, error_messages",
+    [
+        pytest.param(
+            "chunked_uncompressed",
+            False,
+            np.int64,
+            [
+                ""
+            ],
+            id="chunked-uncompressed-false-int64",
+        ),
+        pytest.param(
+            "chunked_uncompressed",
+            True,
+            np.int64,
+            [
+                ""
+            ],
+            id="chunked-uncompressed-true-int64",
+        ),
+        pytest.param(
+            "chunked_compressed",
+            False,
+            np.int64,
+            [
+                ""
+            ],
+            id="chunked-compressed-false-int64",
+        ),
+        pytest.param(
+            "chunked_compressed",
+            True,
+            np.int64,
+            [
+                ""
+            ],
+            id="chunked-compressed-true-int64",
+        ),
+        pytest.param(
+            "contiguous",
+            False,
+            np.int64,
+            [
+                ""
+            ],
+            id="contiguous-false-int64",
+        ),
+        pytest.param(
+            "contiguous",
+            True,
+            np.int64,
+            [
+                ""
+            ],
+            id="contiguous-true-int64",
+        ),
+    ],
+)
+def test_validate_file_positive_int(storage_layout: str, positive: bool, data_type, caplog, error_messages):
+    file_path = os.path.join(f"{storage_layout}.nxs")  # prefix
     # FIX
     with h5py.File(file_path, "w", track_order=True) as h5w:
         trg = "/entry/instrument/electronanalyzer/electron_detector/raw_data"
         for idx, class_name in enumerate(["ENTRY", "INSTRUMENT", "ELECTRONANALYZER", "ELECTRON_DETECTOR"]):
             grp = h5w.create_group(f"{'/'.join(trg.split('/')[0:idx + 2])}")
             grp.attrs["NX_class"] = f"NX{class_name.lower()}"
+
+        axes = ["pixel_y", "pixel_x"]
         grp = h5w.create_group(f"{trg}")
         grp.attrs["NX_class"] = "NXdata"
         grp.attrs["axes"] = axes
         grp.attrs["signal"] = "raw"
 
-        axes = ["pixel_y", "pixel_x"]
         # dst = h5w.create_dataset(f"{trg}/raw", data=np.zeros((1024, 1024), np.float32))
         for idx, axis in enumerate(axes):
             if positive:
-                pixel = np.linspace(0, 1024 - 1, num=1024, endpoint=True, dtype=dtype)
+                pixel = np.linspace(0, 1024 - 1, num=1024, endpoint=True, dtype=data_type)
             else:
-                pixel = np.linspace(-512, 1024 - 1 - 512, num=1024, endpoint=True, dtype=dtype)
+                pixel = np.linspace(-512, 1024 - 1 - 512, num=1024, endpoint=True, dtype=data_type)
             if storage_layout == "chunked_uncompressed":
                 dst = h5w.create_dataset(f"{trg}/{axis}", data=pixel, chunks=(128,))
             elif storage_layout == "chunked_compressed":
@@ -114,5 +202,7 @@ def test_validate_file_positive_int(storage_layout: str, positive: bool, dtype, 
         observed_infos = [
             rec.getMessage() for rec in caplog.records if rec.levelno == logging.INFO
         ]
+        for rec in observed_infos:
+            print(rec)
 
     os.remove(file_path)
