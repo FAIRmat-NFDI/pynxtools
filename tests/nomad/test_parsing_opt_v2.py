e82bf6a182851cf421f71831552265aaa31cca99
lukaspie <lukaspie@github.com>
2026-08-12 12:03:30 +0200

    add REUSE license headers

--- tests/nomad/test_parsing_opt_v2.py
+++ tests/nomad/test_parsing_opt_v2.py
@@ -1,20 +1,9 @@
+# SPDX-FileCopyrightText: The pynxtools Authors
 #
-# Copyright The NOMAD Authors.
+# This file is part of pynxtools.
 #
-# This file is part of NOMAD. See https://nomad-lab.eu for further info.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# SPDX-License-Identifier: Apache-2.0
+
 """v2 port of test_parsing_opt.py: NexusParserV2 with the same storage-layout/dtype matrix.
 
 Group names ("event1"/"image1") are unchanged from v1 — NXDL ``nameType="partial"``
